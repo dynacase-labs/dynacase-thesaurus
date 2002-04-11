4682e64171e277ddf7830f44c64c059f16b3d80e
eric <eric>
2002-04-11 08:57:49 +0000

    original webcalendar 0.9.34

--- freedom/Api/freedom_clean.sql
+++ freedom/Api/freedom_clean.sql
@@ -3,6 +3,6 @@ delete from docvalue where docid not in (select id from doc);
 delete from docvalue where attrid not in (select id from docattr);
 delete from docattr where docid not in (select id from doc);
 delete from fld where dirid not in (select id from doc);
-delete from fld where childid not in (select id from doc); 
+delete from fld where childid not in (select id from doc) and qtype='S'; 
 vacuum full;
 
