a25b5a78728e3590c45f81c9a65831ee24938dcf
eric <eric>
2002-06-18 14:20:58 +0000

    set default values

--- freedom/Zone/Fdl/editbodycard.php
+++ freedom/Zone/Fdl/editbodycard.php
@@ -1,7 +1,7 @@
 <?php
 
 // ---------------------------------------------------------------
-// $Id: editbodycard.php,v 1.1 2002/06/14 08:58:34 eric Exp $
+// $Id: editbodycard.php,v 1.2 2002/06/18 14:20:58 eric Exp $
 // $Source: /home/cvsroot/anakeen/freedom/freedom/Zone/Fdl/Attic/editbodycard.php,v $
 // ---------------------------------------------------------------
 //  O   Anakeen - 2001
@@ -35,94 +35,93 @@ include_once("FDL/editutil.php");
 // -----------------------------------
 function editbodycard(&$action) {
   // -----------------------------------
-  //print "<HR>EDITCARD<HR>";
+    //print "<HR>EDITCARD<HR>";
   // Get All Parameters
-  $docid = GetHttpVars("id",0);        // document to edit
-  $classid = GetHttpVars("classid",0); // use when new doc or change class
-  $dirid = GetHttpVars("dirid",0); // directory to place doc if new doc
-
-
-  // Set the globals elements
-
-  $baseurl=$action->GetParam("CORE_BASEURL");
+    $docid = GetHttpVars("id",0);        // document to edit
+      $classid = GetHttpVars("classid",0); // use when new doc or change class
+	$dirid = GetHttpVars("dirid",0); // directory to place doc if new doc
+	  
+	  
+	  // Set the globals elements
+	    
+	    $baseurl=$action->GetParam("CORE_BASEURL");
   $standurl=$action->GetParam("CORE_STANDURL");
   $dbaccess = $action->GetParam("FREEDOM_DB");
-
-   
-
-
-
-
+  
+  
+  
+  
+  
+  
   
   // ------------------------------------------------------
-  //  new or modify ?
-  if ($docid == 0)
-    {
+    //  new or modify ?
+      if ($docid == 0)    {
+	
+	// new document
+	  switch ($classid) {
+	  case 2:
+	    $action->lay->Set("TITLE", _("new directory"));
+	    break;
+	  case 3:	  
+	  case 4:	  
+	    $action->lay->Set("TITLE", _("new profile"));
+	    break;
+	  default:
+	    $action->lay->Set("TITLE", _("new document"));
+	  }
+	$action->lay->Set("editaction", $action->text("create"));
 
-      // new document
-      switch ($classid) {
-      case 2:
-	$action->lay->Set("TITLE", _("new directory"));
-	break;
-      case 3:	  
-      case 4:	  
-	$action->lay->Set("TITLE", _("new profile"));
-	break;
-      default:
-	$action->lay->Set("TITLE", _("new document"));
-      }
-      $action->lay->Set("editaction", $action->text("create"));
-      if ($classid > 0) {
-	$doc=createDoc($dbaccess,$classid); // the doc inherit from chosen class
-	$doc->id=$classid;
-      }
-     
-    }
-  else
-    {      
-      
-
-      // when modification 
-      $doc= new Doc($dbaccess,$docid);
-      if ($classid == 0)    $classid=$doc->fromid;
-      $err = $doc->CanLockFile()  ;
-      if ($err != "")	$err=$doc->CanUpdateDoc();
-      if ($err != "")   $action->ExitError($err);
-      if (! $doc->isAffected()) $action->ExitError(_("document not referenced"));
-      
-      
-      $action->lay->Set("TITLE", $doc->title);
-      $action->lay->Set("editaction", $action->text("modify"));
-      
-    }
-  
-      // ------------------------------------------------------
-      // build list of class document
-      $query = new QueryDb($dbaccess,"Doc");
-      $query->AddQuery("doctype='C'");
-      
-      $selectclass=array();
-
-      $tclassdoc = $doc->GetClassesDoc($classid);
-      while (list($k,$cdoc)= each ($tclassdoc)) {
-	$selectclass[$k]["idcdoc"]=$cdoc->initid;
-	$selectclass[$k]["classname"]=$cdoc->title;
-	$selectclass[$k]["selected"]="";
-      }
-      
-      // add no inherit for class document
-      if ($doc->doctype=="C") {
-	$selectclass[$k+1]["idcdoc"]="0";
-	$selectclass[$k+1]["classname"]=_("no document type");
-      }
-      $action->lay->SetBlockData("SELECTCLASS", $selectclass);
-  
-   // selected the current class document
-      while (list($k,$cdoc)= each ($selectclass)) {	
-	if ($classid == $selectclass[$k]["idcdoc"]) {	  
-	  $selectclass[$k]["selected"]="selected";
+	if ($classid > 0) {
+	  $cdoc= new Doc($dbaccess,$classid);
+	  $doc=createDoc($dbaccess,$classid); // the doc inherit from chosen class
+	    $doc->id=$classid;
 	}
+	
+      }  else    {      
+	
+	
+	// when modification 
+	  $doc= new Doc($dbaccess,$docid);
+	if ($classid == 0)    $classid=$doc->fromid;
+	$err = $doc->CanLockFile()  ;
+	if ($err != "")	$err=$doc->CanUpdateDoc();
+	if ($err != "")   $action->ExitError($err);
+	if (! $doc->isAffected()) $action->ExitError(_("document not referenced"));
+	
+	
+	$action->lay->Set("TITLE", $doc->title);
+	$action->lay->Set("editaction", $action->text("modify"));
+	
       }
+  
+  // ------------------------------------------------------
+    // build list of class document
+      $query = new QueryDb($dbaccess,"Doc");
+  $query->AddQuery("doctype='C'");
+  
+  $selectclass=array();
+  
+  $tclassdoc = $doc->GetClassesDoc($classid);
+  while (list($k,$icdoc)= each ($tclassdoc)) {
+    $selectclass[$k]["idcdoc"]=$icdoc->initid;
+    $selectclass[$k]["classname"]=$icdoc->title;
+    $selectclass[$k]["selected"]="";
+  }
+  
+  // add no inherit for class document
+    if ($doc->doctype=="C") {
+      $selectclass[$k+1]["idcdoc"]="0";
+      $selectclass[$k+1]["classname"]=_("no document type");
+    }
+  $action->lay->SetBlockData("SELECTCLASS", $selectclass);
+  
+  // selected the current class document
+    while (list($k,$icdoc)= each ($selectclass)) {	
+      if ($classid == $selectclass[$k]["idcdoc"]) {	  
+	$selectclass[$k]["selected"]="selected";
+      }
+    }
   
   
   $action->lay->Set("id", $docid);
@@ -132,14 +131,14 @@ function editbodycard(&$action) {
   
   
   // ------------------------------------------------------
-  // Perform SQL search for doc attributes
-  // ------------------------------------------------------
+    // Perform SQL search for doc attributes
+      // ------------------------------------------------------
 	
 	
 	
 	
 	
-  $bdattr = new DocAttr($dbaccess);
+	$bdattr = new DocAttr($dbaccess);
   
   
   
@@ -154,28 +153,28 @@ function editbodycard(&$action) {
   $nattr = count($listattr); // number of attributes
     
     
-  $k=0; // number of frametext
-  $v=0;// number of value in one frametext
-  $currentFrameId="";
+    $k=0; // number of frametext
+      $v=0;// number of value in one frametext
+	$currentFrameId="";
   $changeframe=false;
   $ih = 0; // index for hidden values
-  $thidden =array();
+    $thidden =array();
   $tableframe=array();
   for ($i=0; $i < $nattr + 1; $i++)
     {
       // Compute value elements
-      if ($i < $nattr)
-	{
+	if ($i < $nattr)
+	  {
 	    
-	  if ($docid > 0) $value = $doc->GetValue($listattr[$i]->id);
-	  else $value="";
+	    if ($docid > 0) $value = $doc->GetValue($listattr[$i]->id);
+	    else $value = $cdoc->GetValue($listattr[$i]->id);
 	    
 	    
-	  if ( $currentFrameId != $listattr[$i]->frameid) {
-	    if ($currentFrameId != "") $changeframe=true;
+	    if ( $currentFrameId != $listattr[$i]->frameid) {
+	      if ($currentFrameId != "") $changeframe=true;
+	    }
+	    
 	  }
-	    
-	}
       
       
       if (($i == $nattr) ||  // to generate final frametext
@@ -198,49 +197,49 @@ function editbodycard(&$action) {
       
       
       //------------------------------
-      // Set the table value elements
-      if (($i < $nattr) && ($listattr[$i]->type != "frame"))
-	{
+	// Set the table value elements
+	  if (($i < $nattr) && ($listattr[$i]->type != "frame"))
+	    {
 	      
-	  $currentFrameId = $listattr[$i]->frameid;
-	  if ( ($listattr[$i]->visibility == "H") || 
-	       ($listattr[$i]->visibility == "R") && (substr_count($listattr[$i]->type,"text") > 0)) {
-	    // special case for hidden values
-	    $thidden[$ih]["hname"]= "_".$listattr[$i]->id;
-	    $thidden[$ih]["hid"]= $listattr[$i]->id;
-	    $thidden[$ih]["hvalue"]=chop(htmlentities($value));
-	    $ih++;
-	  } else {
-	    $tableframe[$v]["value"]=chop(htmlentities($value));
-	    $label = $doc->GetLabel($listattr[$i]->id);
-	    $tableframe[$v]["attrid"]=$listattr[$i]->id;
-	    $tableframe[$v]["name"]=chop("[TEXT:".$label."]");
-	    //$tableframe[$v]["name"]=$action->text($label);
-	    $tableframe[$v]["inputtype"]=getHtmlInput($action, 
-						      $doc->id,
-						      $listattr[$i]->id, 
-						      $listattr[$i]->type, 
-						      $listattr[$i]->visibility, 
-						      $value);
-
-	      
+	      $currentFrameId = $listattr[$i]->frameid;
+	      if ( ($listattr[$i]->visibility == "H") || 
+		  ($listattr[$i]->visibility == "R") && (substr_count($listattr[$i]->type,"text") > 0)) {
+		// special case for hidden values
+		  $thidden[$ih]["hname"]= "_".$listattr[$i]->id;
+		$thidden[$ih]["hid"]= $listattr[$i]->id;
+		$thidden[$ih]["hvalue"]=chop(htmlentities($value));
+		$ih++;
+	      } else {
+		$tableframe[$v]["value"]=chop(htmlentities($value));
+		$label = $doc->GetLabel($listattr[$i]->id);
+		$tableframe[$v]["attrid"]=$listattr[$i]->id;
+		$tableframe[$v]["name"]=chop("[TEXT:".$label."]");
+		//$tableframe[$v]["name"]=$action->text($label);
+		$tableframe[$v]["inputtype"]=getHtmlInput($action, 
+							  $doc->id,
+							  $listattr[$i]->id, 
+							  $listattr[$i]->type, 
+							  $listattr[$i]->visibility, 
+							  $value);
 		
 		
-	    $v++;
 		
-	  }
-	}
+		
+		$v++;
+		
+	      }
+	    }
     }
   
   // Out
     
-  $action->lay->SetBlockData("HIDDENS",$thidden);
+    $action->lay->SetBlockData("HIDDENS",$thidden);
   $action->lay->SetBlockData("TABLEBODY",$frames);
   
   
   if (count( $doc->transitions) > 0) {
     // compute the changed state
-    $fstate = $doc->GetFollowingStates();
+      $fstate = $doc->GetFollowingStates();
     $action->lay->Set("initstatevalue",$doc->state );
     $action->lay->Set("initstatename", $action->text($doc->state) );
     $tstate= array();
