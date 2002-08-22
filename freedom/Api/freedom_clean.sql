b6df411c2549762e762a851db6db00725fb9e43b
eric <eric>
2002-08-22 12:30:49 +0000

    ajout analyze dans vacuum

--- freedom/Api/freedom_clean.sql
+++ freedom/Api/freedom_clean.sql
@@ -5,5 +5,5 @@ delete from docvalue where attrid not in (select id from docattr);
 delete from docattr where docid not in (select id from doc);
 delete from fld where dirid not in (select id from doc);
 delete from fld where childid not in (select id from doc) and qtype='S'; 
-vacuum full;
+vacuum full analyze;
 
