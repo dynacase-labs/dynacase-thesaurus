816f9b2c985ddbc348002b5bfa72c3bc66d33a34
eric <eric>
2003-04-07 16:50:44 +0000

    optimisation refresh

--- freedom/Api/freedom_refresh.php
+++ freedom/Api/freedom_refresh.php
@@ -6,12 +6,11 @@
 
 include_once("FDL/Class.Doc.php");
 
-$className = GetHttpVars("class","-"); // classname filter
 $famId = GetHttpVars("famid",""); // familly filter
 
 
-if (($className == "-") && ($famId == 0)) {
-  print "arg class needed :usage --class=<class name> --famid=<familly id>";
+if  ($famId == 0) {
+  print "arg class needed :usage  --famid=<familly id>";
   return;
 }
 
@@ -36,20 +35,24 @@ if ($famId > 0) {
 $query = new QueryDb($dbaccess,"Doc$famId");
 $query->AddQuery("locked != -1");
 
-if ($className != "-")$query->AddQuery("classname ~* '$className'");
 
       
     
-$table1 = $query->Query();
+$table1 = $query->Query(0,0,"TABLE");
 
      
 if ($query->nb > 0)	{
+
+  printf("\n%d documents to refresh\n", count($table1));
+  $card=count($table1);
+  $doc = createDoc($dbaccess,$famId,false);
 	  while(list($k,$v) = each($table1)) 
 	    {	     
-	      print $v->title . "\n";
-	      $v->refresh();
-	      $v->refreshTitle();
-	      $v->Modify();
+	      $doc->Affect($v);
+	      print $card-$k.")".$doc->title . "\n";
+	      $doc->refresh();
+	      $doc->refreshTitle();
+	      $doc->Modify();
 
 	    }	  
 }      
