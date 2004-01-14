abb5362e5e908bb34f2c73ab991f1e2a6d0d8b45
eric <eric>
2004-01-14 14:22:29 +0000

    add title in window banner

--- freedom/Action/Freedom/histo.php
+++ freedom/Action/Freedom/histo.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Historique view
  *
  * @author Anakeen 2000 
- * @version $Id: histo.php,v 1.5 2003/08/18 15:47:03 eric Exp $
+ * @version $Id: histo.php,v 1.6 2004/01/14 14:22:29 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage GED
@@ -11,34 +11,18 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: histo.php,v 1.5 2003/08/18 15:47:03 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Freedom/histo.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
+
 
 
 include_once("FDL/Class.Doc.php");
 function histo(&$action) 
 {
-  return;
+  $dbaccess = $action->GetParam("FREEDOM_DB");
+  $docid = GetHttpVars("id",0);
+
+  
+  $doc= new Doc($dbaccess,$docid);
+  $action->lay->Set("title",$doc->title);
   
 }
 
