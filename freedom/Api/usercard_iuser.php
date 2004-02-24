47734ca94f71a3313c9307c49d0aeeb36fadb269
eric <eric>
2004-02-24 13:40:58 +0000

    Change to update groups

--- freedom/Api/usercard_iuser.php
+++ freedom/Api/usercard_iuser.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: usercard_iuser.php,v 1.6 2004/02/09 16:43:56 eric Exp $
+ * @version $Id: usercard_iuser.php,v 1.7 2004/02/24 13:40:58 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -51,11 +51,17 @@ if ($query->nb > 0)	{
   printf("\n%d user to update\n", count($table1));
   $card=count($table1);
   $doc = createDoc($dbaccess,$famId,false);
-  while(list($k,$v) = each($table1)) 	    {	     
+  $reste=$card;
+  foreach($table1 as $k=>$v) 	    {
+    $fid=0;
+	
+    $reste--;
     // search already created card
     $title = strtolower($v["lastname"]. " ". $v["firstname"]);
     $mail = getMailAddr($v["id"]);
     // first in IUSER
+    unset($tdoc);
+
     if ($v["isgroup"] == "Y") {
       $filter = array("us_whatid = ".$v["id"]);
       $tdoc = getChildDoc($dbaccess, 0,0,"ALL", $filter,1,"LIST",
@@ -65,13 +71,23 @@ if ($query->nb > 0)	{
       $tdoc = getChildDoc($dbaccess, 0,0,"ALL", $filter,1,"LIST",
 			  getFamIdFromName($dbaccess,"IUSER"));
     }
+    
+
     if (count($tdoc) > 0) {
       
-      $tdoc[0]->refresh();
-      $tdoc[0]->postModify();
+      if (method_exists($tdoc[0],"RefreshGroup")) $tdoc[0]->RefreshGroup();
+      else if (method_exists($tdoc[0],"RefreshDocUser")) $tdoc[0]->RefreshDocUser();
+      //if (method_exists($tdoc[0],"SetGroupMail")) $tdoc[0]->SetGroupMail();
+      //$tdoc[0]->refresh();
+      //$tdoc[0]->postModify();
       $err=$tdoc[0]->modify();
+      
       if ($err != "") print "$err\n";
-      else printf( _("%s updated\n"),$tdoc[0]->title);
+      else {
+	print "$reste)";printf( _("%s updated\n"),$tdoc[0]->title);
+	$fid=$tdoc[0]->id;
+      }
+
       
     } else {
       // search in all usercard same title
@@ -86,15 +102,20 @@ if ($query->nb > 0)	{
 	  if ($tdoc[0]->fromid == getFamIdFromName($dbaccess,"USER")) {
 	    $tdoc[0]->convert(getFamIdFromName($dbaccess,"IUSER"),
 			      array("US_WHATID"=>$v["id"]));
-	    printf( _("%s migrated\n"),$title);
+	    print "$reste)";printf( _("%s migrated\n"),$title);
+	    $fid=$tdoc[0]->id;
 	  }	else {
 	    $udoc= new Doc($dbaccess,$tdoc[0]->id);
 	    $udoc->setValue("US_WHATID",$v["id"]);
 	    $udoc->refresh();
-	    $udoc->postmodify();
+	    $udoc->RefreshDocUser();
 	    $udoc->modify();
-	    printf( _("%s updated\n"),$title);
+	    $fid=$udoc->id;
+	    print "$reste)";printf( _("%s updated\n"),$title);
+	    uset($udoc);
 	  }
+
+
 	}
 	
       } else {
@@ -106,22 +127,29 @@ if ($query->nb > 0)	{
 	  $iuser->refresh();
 	  $iuser->postmodify();
 	  $iuser->modify();
-	  printf( _("%s igroup created\n"),$title);
+	  print "$reste)";printf( _("%s igroup created\n"),$title);
 	} else {
 	  $iuser = createDoc($dbaccess,getFamIdFromName($dbaccess,"IUSER"));
 	  $iuser->setValue("US_WHATID",$v["id"]);
 	  $iuser->Add();
 	  $iuser->refresh();
-	  $iuser->postmodify();
+	  $iuser->RefreshDocUser();
 	  $iuser->modify();
-	  printf( _("%s iuser created\n"),$title);
+	  print "$reste)";printf( _("%s iuser created\n"),$title);
 	}
-	
+	$fid=$iuser->id;
+	unset($iuser);
       }
 
       
     }
-
+   
+    if (($v["fid"] == 0) && ($fid > 0)) {
+      $u= new User("",$v["id"]);
+      $u->fid=$fid;
+      $u->modify();
+	unset($u);
+    }
   }	  
 }      
     
