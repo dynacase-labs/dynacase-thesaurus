652b660efcfa911b458fef37b300a459165e62ba
eric <eric>
2004-02-25 13:26:31 +0000

    Correction restriction folder

--- freedom/Class/Fdl/Class.Dir.php
+++ freedom/Class/Fdl/Class.Dir.php
@@ -3,7 +3,7 @@
  * Folder document definition
  *
  * @author Anakeen 2000 
- * @version $Id: Class.Dir.php,v 1.26 2004/02/12 10:32:09 eric Exp $
+ * @version $Id: Class.Dir.php,v 1.27 2004/02/25 13:26:31 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -101,6 +101,45 @@ Class Dir extends PDir
     $q->Query(0,0,"TABLE","delete from fld where dirid=".$this->id);
   }
 
+
+  /**
+   * virtual method use before insert document in folder
+   * @param int $docid document identificator to insert
+   * @param bool $multiple flag to indicate if the insertion is a part of grouped insertion
+   * @return string error message if not empty the insert will be aborted
+   */
+  function preInsertDoc($docid,$multiple=false) { }  
+
+  /**
+   * virtual method use after insert document in folder
+   * @param int $docid document identificator to insert
+   * @param bool $multiple flag to indicate if the insertion is a part of grouped insertion
+   * @return string error message 
+   */
+  function postInsertDoc($docid,$multiple=false) { }
+  /**
+   * virtual method use after insert multiple document in this folder
+   * @param array $tdocid array of document identificator to insert
+   * @return string error message 
+   */
+  function postMInsertDoc($tdocid) { }
+
+  /**
+   * virtual method use after unlink document in folder
+   * @param int $docid document identificator to unlink
+   * @param bool $multiple flag to indicate if the insertion is a part of grouped insertion
+   * @return string error message if not empty the insert will be aborted
+   */
+  function preUnlinkDoc($docid,$multiple=false) { }  
+
+  /**
+   * virtual method use after unlink document in folder
+   * @param int $docid document identificator to unlink
+   * @param bool $multiple flag to indicate if the insertion is a part of grouped insertion
+   * @return string error message 
+   */
+  function postUnlinkDoc($docid,$multiple=false) { }
+
   /**
    * add a document reference in this folder
    *
@@ -108,21 +147,26 @@ Class Dir extends PDir
    * if mode is static the user see the revision which has been inserted
    * @param int $docid document ident for the insertion
    * @param string $mode latest|static 
+   * @param bool $noprepost if true if the virtuals methods {@link preInsertDoc()} and {@link postInsertDoc()} are not called
    * @return string error message, if no error empty string
    */
-  function AddFile($docid, $mode="latest") {
+  function AddFile($docid, $mode="latest",$noprepost=false) {
     
     // need this privilege
     $err = $this->Control("modify");
     if ($err!= "") return $err;
 
+    // use pre virtual method
+    if (!$noprepost) $err=$this->preInsertDoc($docid);
+    if ($err!= "") return $err;
+    
+
 
     // verify if doc family is autorized
     $doc= new Doc($this->dbaccess, $docid);
-    $ta=$this->getAuthorizedFamilies();
+    
 
-
-    if (! isset($ta[$doc->fromid])) return sprintf(_("Cannot add %s in %s folder, restriction set to add this kind of document"),  $doc->title ,$this->title);
+    if (! $this->isAuthorized($doc->fromid)) return sprintf(_("Cannot add %s in %s folder, restriction set to add this kind of document"),  $doc->title ,$this->title);
 
     $qf = new QueryDir($this->dbaccess);
 
@@ -178,11 +222,129 @@ Class Dir extends PDir
 	}
       }
     }
+
+    // use post virtual method
+    if (!$noprepost) $err=$this->postInsertDoc($docid,false);
+    
     return $err;
   }
   // --------------------------------------------------------------------
+  /**
+   * insert multiple document reference in this folder
+   *
+   * if mode is latest the user always see latest revision 
+   * if mode is static the user see the revision which has been inserted
+   * @param array doc array document  for the insertion
+   * @param string $mode latest|static 
+   * @return string error message, if no error empty string
+   */
+  function InsertMDoc($tdocs, $mode="latest",$noprepost=false) {
+    
+    // need this privilege
+    $err = $this->Control("modify");
+    if ($err!= "") return $err;
+    $tAddeddocids=array();
+
+    // verify if doc family is autorized
+
+    $qf = new QueryDir($this->dbaccess);
+    foreach ($tdocs as $k=>$tdoc) {
+
+      if (! $this->isAuthorized($tdoc["fromid"])) {
+	$err.= "\n".sprintf(_("Cannot add %s in %s folder, restriction set to add this kind of document"),  $tdoc["title"] ,$this->title);
+      } else {
+	switch ($mode) {
+	case "static":
+
+	  $qf->qtype='F'; // fixed document
+	  $docid=$tdoc["id"];
+	  $qf->childid=$tdoc["id"]; // initial doc
+	  break;
+	case "latest":
+	default:
+      
+	  $qf->qtype='S'; // single user query
+	  $docid=$tdoc["initid"];
+	  $qf->childid=$tdoc["initid"]; // initial doc
+    
+	  break;
+	}  
 
 
+	$qf->id="";
+	$qf->dirid=$this->initid; // the reference folder is the initial id
+	$qf->query="";
+	// use post virtual method
+	if (!$noprepost) $err=$this->preInsertDoc($tdoc["initid"],true);
+
+	if ($err == "") {
+	  $err = $qf->Add();
+	  if ($err == "") {
+	    AddLogMsg(sprintf(_("Add %s in %s folder"), $tdoc["title"], $this->title));
+	  
+	    $tAddeddocids[]=$docid;;
+	    // use post virtual method
+	    if (!$noprepost) $err=$this->postInsertDoc($tdoc["initid"],true);
+	  }
+	}
+      }
+    }
+
+
+    // use post virtual method
+    if (!$noprepost) $err.=$this->postMInsertDoc($tAddeddocids);
+
+    return $err;
+  }
+
+  /**
+   * insert multiple static document reference in this folder
+   *
+   * if mode is latest the user always see latest revision 
+   * if mode is static the user see the revision which has been inserted
+   * @param array doc identificator document  for the insertion
+   * @param bool $noprepost if true if the virtuals methods {@link preInsertDoc()} and {@link postInsertDoc()} are not called
+   * @return string error message, if no error empty string
+   */
+  function InsertMSDocId($tdocids,$noprepost=false) {
+    
+    // need this privilege
+    $err = $this->Control("modify");
+    if ($err!= "") return $err;
+    $tAddeddocids=array();
+    $qf = new QueryDir($this->dbaccess);
+    foreach ($tdocids as $k=>$docid) {
+
+
+
+      // if (! isset($ta[$doc->fromid])) return sprintf(_("Cannot add %s in %s folder, restriction set to add this kind of document"),  $doc->title ,$this->title);
+
+
+     
+      $qf->id="";
+      $qf->qtype='S'; // single user query
+      $qf->childid=$docid; // initial doc
+      $qf->dirid=$this->initid; // the reference folder is the initial id
+      $qf->query="";
+      
+      // use post virtual method
+      if (!$noprepost) $err=$this->postInsertDoc($docid,true);
+      if ($err=="") {
+	$err = $qf->Add();
+	if ($err == "") {
+	  AddLogMsg(sprintf(_("Add %d in %s folder"),$docid , $this->title));
+	  $tAddeddocids[]=$docid;
+	  // use post virtual method
+	  if (!$noprepost) $err=$this->postInsertDoc($docid,true);
+	}
+      }
+    }
+
+    // use post virtual method
+    if (!$noprepost) $err=$this->postMInsertDoc($tAddeddocids);
+    
+    return $err;
+  }
   // --------------------------------------------------------------------
   function getQids($docid) {
     // return array of queries id includes in a directory
@@ -224,6 +386,9 @@ Class Dir extends PDir
     $err = $this->Control("modify");
     if ($err!= "") return $err;
 
+    // use pre virtual method
+    $err=$this->preUnlinkDoc($docid);
+    if ($err!= "") return $err;
    
     $qids = $this->getQids($docid);
 
@@ -244,20 +409,34 @@ Class Dir extends PDir
   
     AddLogMsg(sprintf(_("Delete %d in %s folder"), $docid, $this->title));
 
+    // use post virtual method
+    $err=$this->postUnlinkDoc($docid);
   
     return $err;
   }
   // --------------------------------------------------------------------
 
 
-  // $classid : restrict for same usefor families
+  /**
+   * return families that can be use in insertion
+   * @param int $classid : restrict for same usefor families
+   */
   function getAuthorizedFamilies($classid=0) {
     
     if (! isset($this->authfam)) {
       $tfamid = $this->getTValue("FLD_FAMIDS");
       $tfam   = $this->getTValue("FLD_FAM");
     
+      
+
       $allbut=$this->getValue("FLD_ALLBUT");
+     
+      if (($allbut != "1") && ((count($tfamid) == 0) || ((count($tfamid) == 1) && ($tfamid[0]==0)))) {
+	$this->norestrict=true;
+	return;
+      }
+
+      $this->norestrict=false;;
       $tclassdoc=array();
       if ($allbut != "1") {
 	include_once("FDL/Lib.Dir.php");
@@ -266,14 +445,19 @@ Class Dir extends PDir
 
 	while (list($k,$cdoc)= each ($tallfam)) {
 	  $tclassdoc[$cdoc["id"]]=$cdoc;	 
-	  $tclassdoc += $this->GetChildFam($cdoc["id"]);
-	  
+	  //	  $tclassdoc += $this->GetChildFam($cdoc["id"]);	  
 	}
 	// suppress undesirable families
-      
+
 	reset($tfamid);
 	while (list($k,$famid)= each ($tfamid)) {
+	  $tnofam = $this->GetChildFam(intval($famid));
+	  
 	  unset($tclassdoc[intval($famid)]);
+	  foreach ($tnofam as $ka=>$va) {
+	    unset($tclassdoc[intval($ka)]);
+	    
+	  }
 	}
       } else {
 	//add families
@@ -286,9 +470,26 @@ Class Dir extends PDir
       }
       $this->authfam=$tclassdoc;
     }
+    $this->kauthfam = array_keys($this->authfam);
     return $this->authfam;
-  }
+  }  
 
+  /**
+   * return families that can be use in insertion
+   * @param int $classid : restrict for same usefor families
+   */
+  function isAuthorized($classid) {
+    
+    if (! isset($this->norestrict)) {
+      $this->getAuthorizedFamilies();
+
+    }
+    if ($this->norestrict) return true;
+
+    if (isset($this->authfam[$classid])) return true;
+    
+    return false;
+  }
 }
 
 ?>
