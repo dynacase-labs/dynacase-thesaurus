d78e01be9f888ac27e5258043a4eeaee4939246b
eric <eric>
2004-02-25 13:24:55 +0000

    add array default values

--- freedom/Action/Fdl/import_file.php
+++ freedom/Action/Fdl/import_file.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Import documents
  *
  * @author Anakeen 2000 
- * @version $Id: import_file.php,v 1.59 2004/02/05 15:43:22 eric Exp $
+ * @version $Id: import_file.php,v 1.60 2004/02/25 13:24:55 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -11,28 +11,7 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: import_file.php,v 1.59 2004/02/05 15:43:22 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Fdl/import_file.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
+
 
 include_once("FDL/Class.DocFam.php");
 include_once("FDL/Class.DocSearch.php");
@@ -241,7 +220,7 @@ function add_import_file(&$action, $fimport="") {
     break;
     case "DEFAULT":     
       //   $famdoc=$doc->getFamDoc();
-      $doc->setDefValue($data[1],$data[2]);
+      $doc->setDefValue($data[1],str_replace('\n',"\n",$data[2]));
       $msg=sprintf("add default value %s %s",$data[1],$data[2]);
       AddImportLog($action,$msg);
     break;
