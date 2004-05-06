5d53192a79f85b1e991a5eb9477e18b7e7e64b91
eric <eric>
2004-05-06 09:31:20 +0000

    Add control "edit" to change catg

--- freedom/Action/Generic/generic_editcatg.php
+++ freedom/Action/Generic/generic_editcatg.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: generic_editcatg.php,v 1.3 2003/08/18 15:47:03 eric Exp $
+ * @version $Id: generic_editcatg.php,v 1.4 2004/05/06 09:31:20 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -11,28 +11,6 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: generic_editcatg.php,v 1.3 2003/08/18 15:47:03 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Generic/generic_editcatg.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
 
 
 include_once("FDL/Class.Dir.php");
@@ -51,6 +29,9 @@ function generic_editcatg(&$action) {
 
   $action->lay->set("aid",$aid);
   $doc = new Doc($dbaccess, $famid);
+
+  $err = $doc->control("edit"); // need edit permission
+  if ($err != "") $action->exitError($err);
 
   $a = $doc->getAttribute($aid);
   $action->lay->set("fid",$a->docid);
