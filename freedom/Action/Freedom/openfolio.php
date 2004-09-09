ae9c4bc1d1640a56f62b77d843f5bbb20dd49a72
eric <eric>
2004-09-09 12:57:43 +0000

    Add title in folio window bar

--- freedom/Action/Freedom/openfolio.php
+++ freedom/Action/Freedom/openfolio.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Open port folio document
  *
  * @author Anakeen 2000 
- * @version $Id: openfolio.php,v 1.4 2003/08/18 15:47:03 eric Exp $
+ * @version $Id: openfolio.php,v 1.5 2004/09/09 12:57:43 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage GED
@@ -11,28 +11,7 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: openfolio.php,v 1.4 2003/08/18 15:47:03 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Freedom/openfolio.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
+
 
 
 include_once("FDL/Class.Doc.php");
@@ -51,6 +30,7 @@ function openfolio(&$action) {
   $folio = new Doc($dbaccess,$docid);
   $action->lay->Set("dirid", $folio->initid);
   $action->lay->Set("docid", $docid);
+  $action->lay->Set("title", $folio->title);
   
 
 
