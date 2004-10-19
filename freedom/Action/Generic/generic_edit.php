28b37cd92091021c0459aa0589d993c5d3a5d0a1
eric <eric>
2004-10-19 16:10:01 +0000

    Correct no fixed div for IE

--- freedom/Action/Generic/generic_edit.php
+++ freedom/Action/Generic/generic_edit.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: generic_edit.php,v 1.24 2004/10/11 09:19:17 eric Exp $
+ * @version $Id: generic_edit.php,v 1.25 2004/10/19 16:10:01 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -13,7 +13,7 @@
 
 
 // ---------------------------------------------------------------
-// $Id: generic_edit.php,v 1.24 2004/10/11 09:19:17 eric Exp $
+// $Id: generic_edit.php,v 1.25 2004/10/19 16:10:01 eric Exp $
 // $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Generic/generic_edit.php,v $
 // ---------------------------------------------------------------
 //  O   Anakeen - 2001
@@ -129,12 +129,14 @@ function generic_edit(&$action) {
     */
   }
   $action->lay->set("tablefoot","tableborder");
+  $action->lay->set("tablehead","tableborder");
   $action->lay->set("ddivfoot","none");
   if ($action->Read("navigator","")=="NETSCAPE") {
     if (ereg("rv:([0-9.]+).*",$_SERVER['HTTP_USER_AGENT'],$reg)) {
       if (floatval($reg[1] >= 1.6)) {
 	$action->lay->set("ddivfoot","");
-	$action->lay->set("tablefoot","tablefoot");	
+	$action->lay->set("tablefoot","tablefoot");
+	$action->lay->set("tablehead","tablehead");	
       }
     }
     
