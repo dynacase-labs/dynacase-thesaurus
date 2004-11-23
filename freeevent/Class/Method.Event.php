bb48c133f55296164ad454b682bf8d84737f4765
marc <marc>
2004-11-23 16:22:16 +0000

    *** empty log message ***

--- freeevent/Class/Method.Event.php
+++ freeevent/Class/Method.Event.php
@@ -25,6 +25,16 @@ var $eventEndBeginDate;
  */
 var $eventRessources=array();
 
+
+/**
+ * Default calendar view
+ */
+var $eventCalView = "FREEEVENT:EVENTCALVIEW";
+/**
+ * Default planner view
+ */
+var $eventPlanView = "FREEEVENT:EVENTPLANVIEW";
+
 /**
  * get the begin date for the event
  * @return timestamp the date
@@ -62,4 +72,57 @@ function postModify() {
   $this->setEvent();
 }
 
+function eventplanview() {
+  $this->eventViewCommonAttr();
+  $this->lay->Set("ID", $this->GetValue("ID"));
+  $this->lay->Set("COLOR", "red");
+}
+
+function eventCalView() {
+  $this->eventViewCommonAttr();
+}
+
+function eventViewCommonAttr() {
+  
+  $this->lay->Set("TITLE", $this->GetValue("EVT_TITLE"));
+  $dfmt = $this->eventGetDateTimeString("FCAL_DFMT_1", $this->GetValue("EVT_BEGDATE"));
+  $this->lay->Set("BEGIN_DATE", $dfmt);
+  $dfmt = $this->eventGetDateTimeString("FCAL_DFMT_1", $this->GetValue("EVT_ENDDATE"));
+  $this->lay->Set("END_DATE", $dfmt);
+  $this->lay->Set("OWNER",  $this->GetValue("EVT_CREATOR"));
+  $r = $this->GetTValue("EVT_RES");
+  if (count($r)>0) {
+    $t = array();
+    foreach($r as $k=>$v) {
+      if ($v!="") $t[]["R"] = $v;
+    }
+  } else {
+    $t = NULL;
+  }
+  $this->lay->SetBlockData("RESSOURCES", $t);
+}
+
+function eventGetDateTimeString($fmt="",$date) {
+  global $action;
+  $df = $date;
+  if ($fmt!="") {
+    $f = $action->GetParam($fmt, "%x %X");
+    $df = strftime($f, $this->eventDateToTS($date));  
+  }
+  return $df;
+}
+
+function eventDateToTS($d) {
+  $day = substr($d,0,2);
+  $mon = substr($d,3,2);
+  $yea = substr($d,6,4);
+  $hou = substr($d,11,2);
+  $min = substr($d,14,2);
+  $sec = substr($d,17,2);
+  return mktime($hou, $min, $sec, $mon, $day, $yea);
+}
+
+  
+
+  
 ?>
