00da3f0858c62d9cb2469ffb04ceed5ae947424c
eric <eric>
2004-11-26 14:18:50 +0000

    Init dbfree to freedom by default

--- freedom/Class/Fdl/Lib.Util.php
+++ freedom/Class/Fdl/Lib.Util.php
@@ -3,7 +3,7 @@
  * Utilities functions for freedom
  *
  * @author Anakeen 2004
- * @version $Id: Lib.Util.php,v 1.2 2004/11/25 09:07:32 eric Exp $
+ * @version $Id: Lib.Util.php,v 1.3 2004/11/26 14:18:50 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -30,10 +30,10 @@ function getGen($dbaccess) {
  */
 function toIso8601($fdate,$wtz=false) {
   $isoDate="";
-  if (ereg("^([0-9]{2})/([0-9]{2})/([0-9]{4})[ ]?(([0-2][0-9]):([0-9]{2}):?([0-9]{0,2}))? ?([A-Z]{3,4})?$", $fdate,$reg)) {   
+  if (preg_match("/^(\d\d)\/(\d\d)\/(\d\d\d\d)\s(\d\d)?:?(\d\d)?:?(\d\d)?\s+?(\w+)?$/", $fdate,$reg)) {   
     $isoDate=sprintf("%04d-%02d-%02d %02d:%02d:%02d",
-		     $reg[3],$reg[2],$reg[1],$reg[5],$reg[6],$reg[7]);
-    if ($reg[8]!="") $tz=$reg[8];
+		     $reg[3],$reg[2],$reg[1],$reg[4],$reg[5],$reg[6]);
+    if ($reg[8]!="") $tz=$reg[7];
   }
     // ISO 8601
   if ($wtz && $tz) $isoDate.=" ".$tz;
@@ -46,11 +46,11 @@ function toIso8601($fdate,$wtz=false) {
  * @return Date object date
  */
 function toDate($fdate) {
-  
-  if (ereg("^([0-9]{2})/([0-9]{2})/([0-9]{4})[ ]?(([0-2][0-9]):([0-9]{2}):?([0-9]{0,2}))? ?([A-Z]{3,4})?$", $fdate,$reg)) {   
+  //"^([0-9]{2})/([0-9]{2})/([0-9]{4})[ ]?(([0-2][0-9]):([0-9]{2}):?([0-9]{0,2}))? ?([A-Z]{3,4})?$"
+  if (preg_match("/^(\d\d)\/(\d\d)\/(\d\d\d\d)\s(\d\d)?:?(\d\d)?:?(\d\d)?\s+?(\w+)?$/", $fdate,$reg)) {   
     $isoDate=sprintf("%04d-%02d-%02d %02d:%02d:%02d",
-		     $reg[3],$reg[2],$reg[1],$reg[5],$reg[6],$reg[7]);
-    if ($reg[8]!="") $tz=$reg[8];
+		     $reg[3],$reg[2],$reg[1],$reg[4],$reg[5],$reg[6]);
+    if ($reg[8]!="") $tz=$reg[7];
   }
     // ISO 8601
   $d=new Date($isoDate);
@@ -59,7 +59,17 @@ function toDate($fdate) {
   return $d;
 }
 
-function toMinute($d) {
+function DatetoMinute($d) {
   return $d->minute+((Date_Calc::dateToDays($d->day,$d->month,$d->year)*24)+$d->hour)*60;
 }
+function MinutetoDate($n) {
+  $m= fmod($n,1440);
+  $d = floor($n/1440);
+
+  return sprintf("%s, %02d:%02d",Date_Calc::daysToDate($d),
+		 floor($m/60),fmod($m, 60));
+}
+
+
+
 ?>
