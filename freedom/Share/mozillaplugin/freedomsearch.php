b234b742194da3270fa46b41f6fdf8f8bf104523
marc <marc>
2004-11-29 08:44:27 +0000

    *** empty log message ***

---
+++ freedom/Share/mozillaplugin/freedomsearch.php
@@ -0,0 +1,14 @@
+<?php
+include_once("Lib.Http.php");
+$famid = GetHttpVars("famid", 0);
+$latest = GetHttpVars("latest", true);
+$fromdir = GetHttpVars("fromdir", false);
+$sensitive = GetHttpVars("sensitive", false);
+$keyword=GetHttpVars("keyword", "");
+if ($keyword=="") {
+  echo "<h4>You have to enter a keyword !";
+} else {
+  $location="index.php?sole=Y&&app=FREEDOM&action=SEARCH&famid=$famid&latest=$latest&fromdir=$fromdir&sensitive=$sensitive&keyword=$keyword";
+  Header("Location: $location");
+}
+?>
