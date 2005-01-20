be04f1d2646efd477b1dad6ba727705c7cdd2f8f
eric <eric>
2005-01-20 16:45:53 +0000

    Change post-it edit

--- freedom/Class/Fdl/Method.PostIt.php
+++ freedom/Class/Fdl/Method.PostIt.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: Method.PostIt.php,v 1.2 2003/08/18 15:47:04 eric Exp $
+ * @version $Id: Method.PostIt.php,v 1.3 2005/01/20 16:45:53 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -11,33 +11,12 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: Method.PostIt.php,v 1.2 2003/08/18 15:47:04 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Class/Fdl/Method.PostIt.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
 
 
 
   
 var $defaultview= "FDL:VIEWPOSTIT:T";
+var $defaultedit= "FDL:EDITPOSTIT:T";
   
   
 // -----------------------------------
@@ -65,7 +44,9 @@ function viewpostit($target="_self",$ulink=true,$abstract=false) {
   $this->lay->SetBlockData("TEXT",	 $tlaycomment);
 
 }
-
+function editpostit() {
+  $this->editattr();
+}
 function PostModify() {
   $docid= $this->getValue("PIT_IDADOC");
   if ($docid > 0) {
@@ -76,6 +57,37 @@ function PostModify() {
       $doc->modify();
       $doc->enableEditControl();
     }
+  }
+
+  $ncom = $this->getValue("PIT_NCOM");
+  if ($ncom != "") {
+
+    $tcom=$this->getTValue("PIT_COM");
+    $tdate=$this->getTValue("PIT_DATE");
+    $tiduser=$this->getTValue("PIT_IDUSER");
+    $tcolor=$this->getTValue("PIT_COLOR");
+
+    foreach ($tcom as $k=>$v) {
+      if ($v=="") {
+	unset($tcom[$k]);
+	unset($tdate[$k]);
+	unset($tiduser[$k]);
+	unset($tcolor[$k]);
+      }
+    }
+    $tcom[]=$ncom;
+    $tdate[]=$this->getDate();
+    $tiduser[]=$this->getUserId();
+    $tcolor[]=$this->getValue("PIT_NCOLOR");
+
+    $this->setValue("PIT_COM",$tcom);
+    $this->setValue("PIT_DATE",$tdate);
+    $this->setValue("PIT_IDUSER",$tiduser);
+    $this->setValue("PIT_COLOR",$tcolor);
+    $this->deleteValue("PIT_NCOLOR");
+    $this->deleteValue("PIT_NCOM");
+
+    
   }
 }
 
