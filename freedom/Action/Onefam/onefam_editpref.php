2b5a1da322f95acc611c8ee06a094a1ce9a28124
eric <eric>
2005-01-24 16:14:24 +0000

    Comment

--- freedom/Action/Onefam/onefam_editpref.php
+++ freedom/Action/Onefam/onefam_editpref.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Edit preferences for onefam list
  *
  * @author Anakeen 2000 
- * @version $Id: onefam_editpref.php,v 1.7 2005/01/21 17:42:04 eric Exp $
+ * @version $Id: onefam_editpref.php,v 1.8 2005/01/24 16:14:24 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -11,28 +11,7 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: onefam_editpref.php,v 1.7 2005/01/21 17:42:04 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Onefam/onefam_editpref.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
+
 
 
 include_once("FDL/Class.Doc.php");
@@ -54,15 +33,6 @@ function onefam_editpref(&$action,$idsattr="ONEFAM_IDS",$modaction="ONEFAM_MODPR
 
   $openfam=$action->getParam("ONEFAM_FAMOPEN");
   $action->lay->set("openfirst",$openfam);
-  print "<hr>openfam:$openfam";
-
-  $m1=memory_get_usage();
-  print "<br>[".memory_get_usage()."]";
-  $doc = new Doc($dbaccess,128);
-  $m1=memory_get_usage();
-  $doc = new Doc128($dbaccess);
-  $m2=memory_get_usage();
-  print "<br>[".($m2-$m1)."]";
   $doc = new Doc($dbaccess);
 
   $selectclass=array();
