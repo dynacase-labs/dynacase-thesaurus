c23a4e73416805261e09ccc2eda1038fb6dfbeee
eric <eric>
2005-02-01 15:10:49 +0000

    Add needParamerters

--- freeevent/Class/Method.SCalendar.php
+++ freeevent/Class/Method.SCalendar.php
@@ -3,7 +3,7 @@
  * Static calendar methods
  *
  * @author Anakeen 2005
- * @version $Id: Method.SCalendar.php,v 1.2 2005/01/18 08:45:48 eric Exp $
+ * @version $Id: Method.SCalendar.php,v 1.3 2005/02/01 15:10:49 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEEVENT
  */
@@ -15,4 +15,8 @@ var $cviews=array("FREEEVENT:PLANNER","FREEEVENT:VIEWCALENDAR");
 var $defaultedit="FREEEVENT:EDITSCAL";
 var $defaultview="FREEEVENT:PLANNER";
 
+
+function needParameters() {
+  return false;
+}
 ?>
