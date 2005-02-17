73521e61988a58386f70253c2fc2c433368b072d
eric <eric>
2005-02-17 07:52:21 +0000

    Focus in speedsearch

--- freedom/Action/Freedom/speedsearch.php
+++ freedom/Action/Freedom/speedsearch.php
@@ -1,38 +1,15 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Speed Search
  *
  * @author Anakeen 2000 
- * @version $Id: speedsearch.php,v 1.3 2005/02/08 11:34:37 eric Exp $
+ * @version $Id: speedsearch.php,v 1.4 2005/02/17 07:52:21 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage GED
  */
  /**
  */
-
-// ---------------------------------------------------------------
-// $Id: speedsearch.php,v 1.3 2005/02/08 11:34:37 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Freedom/speedsearch.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
 
 
 
@@ -46,7 +23,6 @@ function speedsearch(&$action) {
   // -----------------------------------
 
   $dbaccess = $action->GetParam("FREEDOM_DB");
-
 
   // Get all the params      
   $dir=GetHttpVars("dirid"); // insert search in this folder
