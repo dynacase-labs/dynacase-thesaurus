26e4b892fc7230205a2ca5b578ee74ebd0969dcd
eric <eric>
2005-02-18 15:42:56 +0000

    comment

--- freedom/Zone/Fdl/editframe.php
+++ freedom/Zone/Fdl/editframe.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Generate Layout to edit frame (fieldset)
  *
  * @author Anakeen 2000 
- * @version $Id: editframe.php,v 1.19 2004/11/22 17:37:30 eric Exp $
+ * @version $Id: editframe.php,v 1.20 2005/02/18 15:42:56 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -11,28 +11,7 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: editframe.php,v 1.19 2004/11/22 17:37:30 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Zone/Fdl/editframe.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
+
 
 include_once("FDL/Class.Doc.php");
 include_once("FDL/Class.DocAttr.php");
