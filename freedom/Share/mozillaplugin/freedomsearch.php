915e52da8eae23a6d8d21da2bac6b2da3817c63a
eric <eric>
2005-03-23 10:31:37 +0000

    Correct iso8859-1

--- freedom/Share/mozillaplugin/freedomsearch.php
+++ freedom/Share/mozillaplugin/freedomsearch.php
@@ -4,11 +4,12 @@ $famid = GetHttpVars("famid", 0);
 $latest = GetHttpVars("latest", true);
 $fromdir = GetHttpVars("fromdir", false);
 $sensitive = GetHttpVars("sensitive", false);
+$viewone = GetHttpVars("viewone", "N");
 $keyword=GetHttpVars("keyword", "");
 if ($keyword=="") {
   echo "<h4>You have to enter a keyword !";
 } else {
-  $location="index.php?sole=Y&&app=FREEDOM&action=SEARCH&famid=$famid&latest=$latest&fromdir=$fromdir&sensitive=$sensitive&keyword=$keyword";
+  $location="index.php?sole=Y&&app=FREEDOM&action=SEARCH&famid=$famid&latest=$latest&fromdir=$fromdir&sensitive=$sensitive&viewone=$viewone&keyword=$keyword";
   Header("Location: $location");
 }
 ?>
