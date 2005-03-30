3cb51bdb94f50239a87ba24d9974e147c9cc7272
marc <marc>
2005-03-30 10:15:05 +0000

    0.0.9f

---
+++ wgcal/Families/Method.CallEvent.php
@@ -0,0 +1,16 @@
+
+/**
+ * Generated Header (not documented yet)
+ *
+ * @author Anakeen 2000
+ * @version $Id: Method.CallEvent.php,v 1.1 2005/03/30 10:15:05 marc Exp $
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package FREEDOM
+ * @subpackage
+ */
+
+var $calVResume     = "WGCAL:CALL_ABSTRACT";
+var $calVCard       = "WGCAL:CALL_CARD";
+var $calVLongText   = "WGCAL:CALL_VIEWLTEXT";
+var $calVShortText  = "WGCAL:CALL_VIEWSTEXT";
+
