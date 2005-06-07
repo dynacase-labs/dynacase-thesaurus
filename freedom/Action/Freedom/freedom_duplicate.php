eb7c60d1edef2afb5943d805d8272b051156aae5
eric <eric>
2005-06-07 16:06:24 +0000

    change css call

--- freedom/Action/Freedom/freedom_duplicate.php
+++ freedom/Action/Freedom/freedom_duplicate.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * Duplicate a document
  *
  * @author Anakeen 2000 
- * @version $Id: freedom_duplicate.php,v 1.11 2005/04/05 17:29:38 eric Exp $
+ * @version $Id: freedom_duplicate.php,v 1.12 2005/06/07 16:06:24 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage GED
@@ -11,28 +11,7 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: freedom_duplicate.php,v 1.11 2005/04/05 17:29:38 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Freedom/freedom_duplicate.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
+
 
 
 include_once("FDL/duplicate.php");
