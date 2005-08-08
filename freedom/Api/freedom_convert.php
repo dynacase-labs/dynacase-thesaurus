8ca7f509fd9f5bae87315f74eaa43225f0d913e5
eric <eric>
2005-08-08 16:00:54 +0000

    More controls before convert

--- freedom/Api/freedom_convert.php
+++ freedom/Api/freedom_convert.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: freedom_convert.php,v 1.3 2005/06/28 08:37:46 eric Exp $
+ * @version $Id: freedom_convert.php,v 1.4 2005/08/08 16:00:54 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -13,8 +13,8 @@
 
 
 
-// refreah for a classname
-// use this only if you have changed title attributes
+  // refreah for a classname
+  // use this only if you have changed title attributes
 
 include_once("FDL/Class.Doc.php");
 
@@ -23,25 +23,29 @@ $docid = GetHttpVars("docid",""); // document
 
 
 if (($docid == "") && ($famId == 0)) {
-  print "arg class needed :usage --class=<class name> --famid=<familly id>";
-  return;
-}
+  print "arg class needed :usage --tofamid=<family id> --docid=<doc id to be converted>\n";
+  
+ } else {
 
 
-$appl = new Application();
-$appl->Set("FDL",	   $core);
+  $appl = new Application();
+  $appl->Set("FDL",	   $core);
 
 
-$dbaccess=$appl->GetParam("FREEDOM_DB");
-if ($dbaccess == "") {
-  print "Freedom Database not found : param FREEDOM_DB";
-  exit;
-}
+  $dbaccess=$appl->GetParam("FREEDOM_DB");
+  if ($dbaccess == "") {
+    print "Freedom Database not found : param FREEDOM_DB";
+    exit;
+  }
 
 
-$doc= new_Doc($dbaccess, $docid);
-$doc->convert($famId);
-print $doc->title. " converted";
-    
+  $doc= new_Doc($dbaccess, $docid);
+  if ($doc->isAffected()) {
+    if ($doc->convert($famId))  print $doc->title. " converted";
+    else print $doc->title. " NOT converted";
+  } else {    
+    print "document  $docid not found";
+  }
+ }
 
 ?>
