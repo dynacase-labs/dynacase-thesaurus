76d327c6e5f01888b5e8ee8a97e7e5ac36605948
eric <eric>
2005-09-07 10:43:26 +0000

    Correct for deported database

--- freedom/Api/freedom_groups.php
+++ freedom/Api/freedom_groups.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: freedom_groups.php,v 1.8 2005/06/28 08:37:46 eric Exp $
+ * @version $Id: freedom_groups.php,v 1.9 2005/09/07 10:43:26 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -32,13 +32,13 @@ if ($dbaccess == "") {
 }
 
 $doc = new_Doc($dbaccess);
-$dbname=getDbName($dbaccess);
-$dbuser=getDbUser($dbaccess);
-$dbank=getDbName(getDbAccess());
+$dbfree=php2DbSql($dbaccess);
+$dbankcoord=php2DbSql(getDbAccess(),false);
+$dbankname=getDbName(getDbAccess());
 
-system("echo 'delete from groups;delete from docperm where upacl=0 and unacl=0;update docperm set cacl=0 where cacl != 0;; ' | psql $dbname $dbuser");
-system("pg_dump -a --disable-triggers -t groups $dbank -U $dbuser | psql $dbname $dbuser");
-system("echo 'vacuum  docperm;vacuum  groups' | psql $dbname $dbuser");
+system("echo 'delete from groups;delete from docperm where upacl=0 and unacl=0;update docperm set cacl=0 where cacl != 0;' | psql $dbfree");
+system("pg_dump -a --disable-triggers -t groups $dbankcoord $dbankname | psql $dbfree");
+system("echo 'vacuum  docperm;vacuum  groups' | psql $dbfree");
 //system("echo 'select getuperm(userid, docid) from docperm' | psql freedom anakeen");
 
 
