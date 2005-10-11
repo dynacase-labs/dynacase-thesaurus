3e2f535fd53ca3e670d8933ebe79f2f4e730cd73
eric <eric>
2005-10-11 14:20:12 +0000

    Correct attributes search

--- freedom/Action/Freedom/freedom_applybatch.php
+++ freedom/Action/Freedom/freedom_applybatch.php
@@ -3,7 +3,7 @@
  * Use to help to the construction of batch document
  *
  * @author Anakeen 2005
- * @version $Id: freedom_applybatch.php,v 1.4 2005/10/05 14:37:58 eric Exp $
+ * @version $Id: freedom_applybatch.php,v 1.5 2005/10/11 14:20:12 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage GED
@@ -32,23 +32,17 @@ function freedom_applybatch(&$action) {
   $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/subwindow.js");
  
   $tb=$bdoc->getChildFam();
-  $q=new QueryDb($dbaccess, "docAttr");
   foreach ($tb as $k=>$v) {
     $tb[$k]["iconsrc"]=$bdoc->getIcon($v["icon"]);
 
     $adoc="Adoc".$v["id"];
-    $fa=new ADoc$adoc;
-    print_r2($fa);
-
-    $q->resetQuery();
-    $q->AddQuery("docid=".$v["id"]);
-    $q->AddQuery("type='action'");
-    $q->AddQuery("docid=".$v["id"]);
-    $la=$q->Query(0,0,"TABLE");
+    include_once("FDLGEN/Class.Doc".$v["id"].".php");
+    $fa=new $adoc;
+    $la=$fa->getActionAttributes();
     $ta=array();
     if ($la) {
       foreach ($la as $ka=>$va) {
-	$ta[]=$va["labeltext"];
+	$ta[]=$va->labelText;
       }
     }
     $tb[$k]["actions"]=implode(",<br>",$ta);
