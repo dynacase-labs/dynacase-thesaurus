660f372df6b275512d1b17c5c7c822e8609d5553
marc <marc>
2005-10-12 07:33:49 +0000

    get http var version

--- wgcal/OSync/check_version.php
+++ wgcal/OSync/check_version.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2005
- * @version $Id: check_version.php,v 1.4 2005/06/21 09:50:21 marc Exp $
+ * @version $Id: check_version.php,v 1.5 2005/10/12 07:33:49 marc Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WGCAL
  * @subpackage SYNC
@@ -13,7 +13,7 @@
 include_once("Lib.WgcalSync.php");
 
 $action = WSyncAuthent();
-
+if ($version >= $action->GetParam("WGCAL_SYNCVERSION","0")) print ("NOERROR\n");$version = GetHttpVars("version", "0");
 header ("Content-Type: text/plain");
 if ($version >= $action->GetParam("WGCAL_SYNCVERSION","0")) print ("NOERROR\n");
 else print ("ERROR\n");
