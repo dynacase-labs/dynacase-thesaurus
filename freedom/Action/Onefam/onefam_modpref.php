220df1bf181537ac42822fd315e3a62efda62a58
eric <eric>
2005-10-31 13:07:14 +0000

    Correct for master pref family

--- freedom/Action/Onefam/onefam_modpref.php
+++ freedom/Action/Onefam/onefam_modpref.php
@@ -3,7 +3,7 @@
  * validate user or master choosen families
  *
  * @author Anakeen 2000 
- * @version $Id: onefam_modpref.php,v 1.6 2005/01/21 17:42:04 eric Exp $
+ * @version $Id: onefam_modpref.php,v 1.7 2005/10/31 13:07:14 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -24,10 +24,13 @@ function onefam_modpref(&$action,$idsattr="ONEFAM_IDS")
   $idsfam = $action->GetParam($idsattr);
   $idsfam = implode(",",$tidsfam);
 
-  if ($idsattr=="ONEFAM_IDS")  $action->parent->param->Set($idsattr,$idsfam,PARAM_USER.$action->user->id,$action->parent->id);
-  else $action->parent->param->Set($idsattr,$idsfam,PARAM_APP,$action->parent->id);
-	  
-  $action->parent->param->Set("ONEFAM_FAMOPEN",$openfam,PARAM_USER.$action->user->id,$action->parent->id);
+  if ($idsattr=="ONEFAM_IDS") {
+    $action->parent->param->Set($idsattr,$idsfam,PARAM_USER.$action->user->id,$action->parent->id);
+    $action->parent->param->Set("ONEFAM_FAMOPEN",$openfam,PARAM_USER.$action->user->id,$action->parent->id);
+  } else {
+    $action->parent->param->Set($idsattr,$idsfam,PARAM_APP,$action->parent->id);	  
+    $action->parent->param->Set("ONEFAM_FAMOPEN",$openfam,PARAM_APP,$action->parent->id);
+  }
       
   redirect($action,GetHttpVars("app"),"ONEFAM_LIST");
   
