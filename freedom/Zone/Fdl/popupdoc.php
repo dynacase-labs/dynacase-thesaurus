967c81904e3e80f0702ee3a989df18bd2e622844
eric <eric>
2006-04-25 09:55:32 +0000

    Add icon in popup

--- freedom/Zone/Fdl/popupdoc.php
+++ freedom/Zone/Fdl/popupdoc.php
@@ -3,7 +3,7 @@
  * Specific menu for family
  *
  * @author Anakeen 2000 
- * @version $Id: popupdoc.php,v 1.2 2006/04/21 15:11:50 eric Exp $
+ * @version $Id: popupdoc.php,v 1.3 2006/04/25 09:55:32 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -29,27 +29,35 @@ function popupdoc(&$action,$tlink,$tsubmenu) {
   $action->lay->Set("SEP",false);
 
 
+  $useicon=false;
   foreach ($tlink as $k=>$v) {
     if ($v["visibility"]==POPUP_INVISIBLE) unset($tlink[$k]);
     else {
-    $tlink[$k]["descr"]=utf8_encode($v["descr"]);
-    $tlink[$k]["tconfirm"]=utf8_encode($v["tconfirm"]);
-    if ((!isset($v["idlink"])) || ($v["idlink"]=="")) $tlink[$k]["idlink"]=$k;
-    if ((!isset($v["target"])) || ($v["target"]=="")) $tlink[$k]["target"]=$k;
-    $tlink[$k]["smid"]="";
-    if ((isset($v["submenu"])) && ($v["submenu"]!="")) {
-      $smid=base64_encode($v["submenu"]);
-      $tlink[$k]["smid"]=$smid;
-      if (! isset($tsubmenu[$smid])) {
-	 $tsubmenu[$smid]=array("idmenu"=>$smid,
-				"labelmenu"=>utf8_encode($v["submenu"]));
+      if ((!isset($v["icon"])) || ($v["icon"]=="")) {
+	$tlink[$k]["icon"]="Images/none.gif";
+      } else {
+	$useicon=true;
       }
-    }
+      
+      $tlink[$k]["descr"]=utf8_encode($v["descr"]);
+      $tlink[$k]["tconfirm"]=utf8_encode($v["tconfirm"]);
+      if ((!isset($v["idlink"])) || ($v["idlink"]=="")) $tlink[$k]["idlink"]=$k;
+      if ((!isset($v["target"])) || ($v["target"]=="")) $tlink[$k]["target"]=$k;
+      $tlink[$k]["smid"]="";
+      if ((isset($v["submenu"])) && ($v["submenu"]!="")) {
+	$smid=base64_encode($v["submenu"]);
+	$tlink[$k]["smid"]=$smid;
+	if (! isset($tsubmenu[$smid])) {
+	  $tsubmenu[$smid]=array("idmenu"=>$smid,
+				 "labelmenu"=>utf8_encode($v["submenu"]));
+	}
+      }
     }
   }
 
           
 
+  $action->lay->Set("ICONS",$useicon);
   $action->lay->SetBlockData("ADDLINK",$tlink);
   $action->lay->SetBlockData("SUBMENU",$tsubmenu);
   $action->lay->SetBlockData("SUBDIVMENU",$tsubmenu);
