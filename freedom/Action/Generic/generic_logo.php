a438a997c518d5d521ba4439f65caecff0e68f30
eric <eric>
2006-06-14 16:24:44 +0000

    Correct for use addjsref

--- freedom/Action/Generic/generic_logo.php
+++ freedom/Action/Generic/generic_logo.php
@@ -3,7 +3,7 @@
  * Display logo
  *
  * @author Anakeen 2000 
- * @version $Id: generic_logo.php,v 1.6 2005/06/28 08:37:46 eric Exp $
+ * @version $Id: generic_logo.php,v 1.7 2006/06/14 16:24:44 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -18,7 +18,8 @@ include_once("GENERIC/generic_util.php");
 function generic_logo(&$action) 
 {
     $action->lay->Set("apptitle","");
-
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/geometry.js");
+ 
     $famid = getDefFam($action);
     if ($famid > 0) {
       $dbaccess = $action->GetParam("FREEDOM_DB");
