20b63469eb6ee4422f7727ec8dbd0f742dce1825
eric <eric>
2006-07-27 16:13:45 +0000

    Special interface for edit mail

---
+++ freedom/Class/Fdl/Method.Mail.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * INterface to send mail
+ *
+ * @author Anakeen 2000 
+ * @version $Id: Method.Mail.php,v 1.1 2006/07/27 16:15:40 eric Exp $
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package FREEDOM
+ * @subpackage GED
+ */
+ /**
+ */
+
+var $defaultedit= "FDL:EDITMAILDOC";
+
+
+function editmaildoc() {
+  $this->editattr();
+}
+?>
