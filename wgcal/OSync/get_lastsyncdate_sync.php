ec3d804f7469e81cce92cd2f9568156a3d2291bb
caroline <caroline>
2007-01-04 16:40:37 +0000

    correct bug of date null

--- wgcal/OSync/get_lastsyncdate_sync.php
+++ wgcal/OSync/get_lastsyncdate_sync.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2005
- * @version $Id: get_lastsyncdate_sync.php,v 1.4 2005/06/21 09:50:21 marc Exp $
+ * @version $Id: get_lastsyncdate_sync.php,v 1.5 2007/01/04 16:40:37 caroline Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package WGCAL
  * @subpackage SYNC
@@ -17,6 +17,14 @@ $action = WSyncAuthent();
 
 $db = WSyncGetAdminDb();
 $syncdate = new WSyncDate($db, $action->parent->user->fid);
+
+if ($syncdate->server_date=="") {
+$syncdate->server_date=0;
+}
+
+if ( $syncdate->outlook_date=="") {
+$syncdate->outlook_date=0;
+}
 
 print WSyncTs2Outlook($syncdate->server_date)." ".WSyncTs2Outlook($syncdate->outlook_date);
 
