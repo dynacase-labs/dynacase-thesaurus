1e43c54071a457a266ff8ad940ec2551cba5abbf
eric <eric>
2007-01-04 16:44:15 +0000

    resize icone

--- freedom/Action/Generic/generic_logo.php
+++ freedom/Action/Generic/generic_logo.php
@@ -3,7 +3,7 @@
  * Display logo
  *
  * @author Anakeen 2000 
- * @version $Id: generic_logo.php,v 1.7 2006/06/14 16:24:44 eric Exp $
+ * @version $Id: generic_logo.php,v 1.8 2007/01/04 16:44:23 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -19,6 +19,7 @@ function generic_logo(&$action)
 {
     $action->lay->Set("apptitle","");
     $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/geometry.js");
+    $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/resizeimg.js");
  
     $famid = getDefFam($action);
     if ($famid > 0) {
