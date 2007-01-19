e24723058be7d67d03504e9d39c20ef8fa5bcd4d
eric <eric>
2007-01-19 16:26:59 +0000

    Add $to parameter to send import report by mail

--- freedom/Api/freedom_import.php
+++ freedom/Api/freedom_import.php
@@ -3,7 +3,7 @@
  * importation of documents
  *
  * @author Anakeen 2002
- * @version $Id: freedom_import.php,v 1.7 2006/08/15 13:56:10 eric Exp $
+ * @version $Id: freedom_import.php,v 1.8 2007/01/19 16:26:59 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage WSH
@@ -16,6 +16,7 @@ global $appl,$action;
 
 include_once("FDL/import_file.php");
 
+$to = GetHttpVars("to"); 
 if (GetHttpVars("htmlmode") == "Y") {
   // mode HTML
   $appl=new Application();
@@ -23,8 +24,26 @@ if (GetHttpVars("htmlmode") == "Y") {
 
   $action->Set("FREEDOM_IMPORT",$appl);
 
+  $out= ($action->execute());
+  if ($to) {
+    include_once("FDL/sendmail.php");
+    
+    $themail = new Mail_mime();    
+    $themail->setHTMLBody($out,false);
+    
+    $from=getMailAddr($action->user->id);
+    if ($from == "")  $from = getParam('SMTP_FROM');
+    if ($from == "")  $from = $action->user->login;
 
-  print ($action->execute());
+    $subject=sprintf(_("result of import  %s"), basename(GetHttpVars("file")));
+    $err=sendmail($to,$from,$cc,$bcc,$subject,$themail);
+    if ($err) print "Error:$err\n";
+    
+  } else {
+    print $out;
+  }
+
+  
 } else {
   // mode TEXT
   $appl=new Application();
