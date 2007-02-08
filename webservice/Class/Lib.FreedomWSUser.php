d68303189d71b9155c85d9e66e9dc69a21f899cd
eric <eric>
2007-02-08 14:19:59 +0000

    Add change_password and getAvailableActions

--- webservice/Class/Lib.FreedomWSUser.php
+++ webservice/Class/Lib.FreedomWSUser.php
@@ -1,20 +1,51 @@
 <?php
+
+
+  /** change password for a user
+   * @param string $login the login of the user
+   * @param string $password the new password in clear text
+   * @return string error message if one else empty string if OK
+   */
+function changePassword($login,$password) {
+  global $action;
+  $dbfreedom=  _initFreedom();
+  $uid = _getUserFid();
+  if (! $action->hasPermission("FUSERS_MASTER","FUSERS")) return sprintf(_("permission FUSERS_MASTER:FUSERS needed\n"));
+
+    $u = new User();
+    if ($u->SetLoginName($login)) {
+      if ($u->fid > 0) {	
+	$du=new_doc($dbfreedom,$u->fid);
+	if ($du->isAlive()) {
+	  $err=$du->control("edit");
+	  if ($err != "") return $err;
+	}
+      }
+      $u->password_new=$password;
+      $err=$u->modify();
+      if ($err != "") return $err;
+    } else {
+      return sprintf(_("login %s not found\n"),$login);
+    }
+  }
+
+
 /**
    * get list of avalaibel application for a user
    * @param string $login the login of the user
    * @return array list of application
    */
    function getAvailableApplication($login) {
-  //print "<H1>COUCOU</H1>";
+  
     global $action;
     _initFreedom();
 
+    $action->user=new User();
 
-
-    return array("coucou"=>"test");
+    //    return serialize(array("coucou"=>"test"));
     if ($action->user->SetLoginName($login)) {
       
-      return $action->GetAvailableApplication();
+      return serialize($action->GetAvailableApplication());
     } else {
       return sprintf(_("login %s not found\n"),$login);
     }    
--- webservice/Tools/test_docUser.php
+++ webservice/Tools/test_docUser.php
@@ -7,19 +7,30 @@ toolhead();
 $c = toolinitsoap("http://localhost/freedomWS","admin","anakeen");
 
 // $param = array('docid' => 1323, 'docrev' => '1');
+/*
 $param = array('login' => 'eric');
 $r = $c->call('getAvailableApplication', $param);
 
 if ($r) {
   print ($c->response);
-
   print "<hr><pre>";
-  print str_replace("<","&lt;",base64_decode($r));
+  print_r(unserialize($r));
   print "</pre>";
  } else {
 toolresult($c, $r);
  }
-//toolresult($c, $r);
+*/
+$param = array('login' => 'jean.groget',"password"=>'coucou');
+$r = $c->call('changePassword', $param);
+
+if ($r) {
+  print ($c->response);
+  print "<hr><pre>";
+  print_r(unserialize($r));
+  print "</pre>";
+ } else {
+toolresult($c, $r);
+ }
 
 exit;
 
