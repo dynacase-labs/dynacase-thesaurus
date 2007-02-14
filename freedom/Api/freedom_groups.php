79be0ad926b9d78f07efa556c96872528e559da5
eric <eric>
2007-02-14 15:52:18 +0000

    Optimization of copy by suppress index and recreate after

--- freedom/Api/freedom_groups.php
+++ freedom/Api/freedom_groups.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: freedom_groups.php,v 1.11 2006/11/16 16:45:12 eric Exp $
+ * @version $Id: freedom_groups.php,v 1.12 2007/02/14 15:52:18 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -36,9 +36,13 @@ $dbfree=php2DbSql($dbaccess);
 $dbankcoord=php2DbSql(getDbAccess(),false);
 $dbankname=getDbName(getDbAccess());
 
-system("echo 'delete from groups;' | psql $dbfree");
+system("echo 'DROP INDEX groups_idx2;DROP INDEX groups_idx1;delete from groups;' | psql $dbfree");
+
 system("echo 'delete from docperm where upacl=0 and unacl=0;update docperm set cacl=0 where cacl != 0;' | psql $dbfree");
+
 system("pg_dump -a --disable-triggers -t groups $dbankcoord $dbankname | psql $dbfree");
+system("echo 'CREATE unique INDEX groups_idx2 on groups(iduser,idgroup);CREATE INDEX groups_idx1 on  groups(iduser);' | psql $dbfree");
+
 //system("echo 'vacuum  docperm;vacuum  groups' | psql $dbfree");
 //system("echo 'select getuperm(userid, docid) from docperm' | psql freedom anakeen");
 
