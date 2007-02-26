0e27e760fa731cd48411365819ec912fd0bc2ead
eric <eric>
2007-02-26 14:10:05 +0000

    Correct to restore initial copy for group table : need detect number of users to use other copy mode

--- freedom/Api/freedom_groups.php
+++ freedom/Api/freedom_groups.php
@@ -3,7 +3,7 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen 2000 
- * @version $Id: freedom_groups.php,v 1.12 2007/02/14 15:52:18 eric Exp $
+ * @version $Id: freedom_groups.php,v 1.13 2007/02/26 14:10:05 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -36,12 +36,13 @@ $dbfree=php2DbSql($dbaccess);
 $dbankcoord=php2DbSql(getDbAccess(),false);
 $dbankname=getDbName(getDbAccess());
 
-system("echo 'DROP INDEX groups_idx2;DROP INDEX groups_idx1;delete from groups;' | psql $dbfree");
+$big=false; // need to set to true when table count > 20000
+if ($big) system("echo 'DROP INDEX groups_idx2;DROP INDEX groups_idx1;' | psql $dbfree");
 
-system("echo 'delete from docperm where upacl=0 and unacl=0;update docperm set cacl=0 where cacl != 0;' | psql $dbfree");
-
+system("echo 'delete from groups;delete from docperm where upacl=0 and unacl=0;update docperm set cacl=0 where cacl != 0;' | psql $dbfree");
 system("pg_dump -a --disable-triggers -t groups $dbankcoord $dbankname | psql $dbfree");
-system("echo 'CREATE unique INDEX groups_idx2 on groups(iduser,idgroup);CREATE INDEX groups_idx1 on  groups(iduser);' | psql $dbfree");
+
+if ($big) system("echo 'CREATE unique INDEX groups_idx2 on groups(iduser,idgroup);CREATE INDEX groups_idx1 on  groups(iduser);' | psql $dbfree");
 
 //system("echo 'vacuum  docperm;vacuum  groups' | psql $dbfree");
 //system("echo 'select getuperm(userid, docid) from docperm' | psql freedom anakeen");
