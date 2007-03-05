3d31b376f1a61a1b4be6d69a12d1fb87eaf4536c
eric <eric>
2007-03-05 16:11:21 +0000

    New logo for FREEDOM

--- freedom/Action/Freedom/freedom_logo.php
+++ freedom/Action/Freedom/freedom_logo.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Generated Header (not documented yet)
+ * View logo
  *
  * @author Anakeen 2000 
- * @version $Id: freedom_logo.php,v 1.4 2003/08/18 15:47:03 eric Exp $
+ * @version $Id: freedom_logo.php,v 1.5 2007/03/05 16:11:21 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage GED
@@ -11,33 +11,12 @@
  /**
  */
 
-// ---------------------------------------------------------------
-// $Id: freedom_logo.php,v 1.4 2003/08/18 15:47:03 eric Exp $
-// $Source: /home/cvsroot/anakeen/freedom/freedom/Action/Freedom/freedom_logo.php,v $
-// ---------------------------------------------------------------
-//  O   Anakeen - 2001
-// O*O  Anakeen development team
-//  O   dev@anakeen.com
-// ---------------------------------------------------------------
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at
-//  your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// ---------------------------------------------------------------
 
 
 
 function freedom_logo(&$action) 
 {
+  $action->parent->AddJsRef($action->GetParam("CORE_JSURL")."/resizeimg.js");
   $action->lay->Set("appicon",$action->GetImageUrl($action->parent->icon));
   $action->lay->Set("apptitle",$action->parent->description);
 
