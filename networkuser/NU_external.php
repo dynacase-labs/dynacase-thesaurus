6b0669d419087a10fe6680519522bb29dfbd907f
eric <eric>
2007-03-06 10:04:08 +0000

    Correct constraint to login attribute

--- networkuser/NU_external.php
+++ networkuser/NU_external.php
@@ -3,16 +3,18 @@ include_once("NU/Lib.NU.php");
   //function searchLDAPinfo(LDAP_GETINFO):US_LOGIN,US_FNAME,US,LNAME
 function searchLDAPinfo($login) {
   $err=searchLDAPFromLogin($login,false,$tinfo);
-  $conf=getLDAPconf(getParam("NU_LDAP_KIND"));
+  if ($err == "") {
+    $conf=getLDAPconf(getParam("NU_LDAP_KIND"));
+    $tout=array();
+    foreach ($tinfo as $k=>$v) {
+      $login=utf8_decode($v[$conf["LDAP_USERLOGIN"]]);
+      $fn=utf8_decode($v["givenName"]);
+      $ln=utf8_decode($v["sn"]);
 
-  print_r2($tinfo);
-  foreach ($tinfo as $k=>$v) {
-    $login=utf8_decode($v[$conf["LDAP_USERLOGIN"]]);
-    $fn=utf8_decode($v["givenName"]);
-    $ln=utf8_decode($v["sn"]);
-
-    $tout[]=array($login,$login,$fn,$ln);
+      $tout[]=array($login,$login,$fn,$ln);
+    }
   }
+  if ($err) return $err;
   return $tout;
 }
 ?>
