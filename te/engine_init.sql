25eb5d286613414cdf743e3c7d7a9d088cd3ad7a
eric <eric>
2007-06-14 15:49:06 +0000

    Add mime for old openoffice documents

--- te/engine_init.sql
+++ te/engine_init.sql
@@ -1,5 +1,13 @@
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'text', '/usr/share/pear/TE/engines/txt2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.oasis.opendocument.text', '/usr/share/pear/TE/engines/ooo2txt', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.oasis.opendocument.presentation', '/usr/share/pear/TE/engines/ooo2txt', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.oasis.opendocument.spreadsheet', '/usr/share/pear/TE/engines/ooo2txt', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.oasis.opendocument.spreadsheet', '/usr/share/pear/TE/engines/ooo2txt', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.sun.xml.writer', '/usr/share/pear/TE/engines/ooo2txt', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.sun.xml.calc', '/usr/share/pear/TE/engines/ooo2txt', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.sun.xml.impress', '/usr/share/pear/TE/engines/ooo2txt', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.ms-powerpoint', '/usr/share/pear/TE/engines/ooo2txt', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.ms-excel', '/usr/share/pear/TE/engines/ooo2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/msword', '/usr/share/pear/TE/engines/ooo2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/pdf', '/usr/share/pear/TE/engines/pdf2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'text/html', '/usr/share/pear/TE/engines/html2txt', NULL);
