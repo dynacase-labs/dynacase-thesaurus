f96e6fcee7b05490bd08129186e8161759328408
eric <eric>
2007-06-20 13:19:54 +0000

    Suppress parenthesis to avoid scientific notation in timestamp

--- wgcal/Families/Method.RendezVous.php
+++ wgcal/Families/Method.RendezVous.php
@@ -873,7 +873,7 @@ function EventSetDate($dstart, $dend, $type)
   
   $start_H = gmdate("H", $dstart);
   $start_M = gmdate("i", $dstart);
-  $this->lay->set("START", ($dstart*1000));
+  $this->lay->set("START", $dstart*1000);
   $this->lay->set("STARTsec", $dstart);
   $this->lay->set("STARTREAD", ucwords(strftime("%a %d %b %Y", $dstart)));
   $th = array();
