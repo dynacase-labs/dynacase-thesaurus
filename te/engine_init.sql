22575115b9d93afc2991cff6626dccb207ec437c
eric <eric>
2007-06-20 16:17:46 +0000

    Suppress double

--- te/engine_init.sql
+++ te/engine_init.sql
@@ -2,7 +2,6 @@ INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'text', '/u
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.oasis.opendocument.text', '/usr/share/pear/TE/engines/ooo2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.oasis.opendocument.presentation', '/usr/share/pear/TE/engines/ooo2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.oasis.opendocument.spreadsheet', '/usr/share/pear/TE/engines/ooo2txt', NULL);
-INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.oasis.opendocument.spreadsheet', '/usr/share/pear/TE/engines/ooo2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.sun.xml.writer', '/usr/share/pear/TE/engines/ooo2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.sun.xml.calc', '/usr/share/pear/TE/engines/ooo2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/vnd.sun.xml.impress', '/usr/share/pear/TE/engines/ooo2txt', NULL);
