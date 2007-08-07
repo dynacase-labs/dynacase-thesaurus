68998278fc0cf409a0f7bfa4a08f627de037d952
eric <eric>
2007-08-07 10:24:43 +0000

    Add menu to affect local group to network user

--- networkuser/Class/Method.LDAPUser.php
+++ networkuser/Class/Method.LDAPUser.php
@@ -3,7 +3,7 @@
  * Active Directory User manipulation
  *
  * @author Anakeen 2007
- * @version $Id: Method.LDAPUser.php,v 1.8 2007/03/07 15:02:41 eric Exp $
+ * @version $Id: Method.LDAPUser.php,v 1.9 2007/08/07 10:24:43 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM-AD
  */
@@ -17,7 +17,14 @@ var $defaultcreate="NU:NU_EDIT"; // use default view
 
 
 function postModify() {
-  // nothing
+  // not call parent
+  $err=$this->setGroups(); 
+  if ($err=="") {
+      $err=$this->RefreshDocUser();// refresh from core database 
+      
+      //$errldap=$this->RefreshLdapCard();
+      //if ($errldap!="") AddWarningMsg($errldap);
+    } 
 }
 
 
