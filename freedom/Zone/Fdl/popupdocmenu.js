1bf456954b3b66255a6cecd3c001d62d9aa8be92
eric <eric>
2007-08-13 15:47:59 +0000

    Workaround popupmenu for IE (event cloning system)

--- freedom/Zone/Fdl/popupdocmenu.js
+++ freedom/Zone/Fdl/popupdocmenu.js
@@ -1,10 +1,28 @@
 
 var POPMENUINPROGRESSELT=false;
 var POPMENUINPROGRESSEVENT=false;
+
+function cloneEvent(e) {
+  var c=new Object();
+  var names="";
+  for (var name in e) {
+   try {
+     c[name]=e[name];
+   }
+   catch (ex) {
+     c[name]=false;
+   }
+  }
+  return c;
+}
+
 function godocmenu(event,o) {
-  if (! event) event=window.event;
+  if (window.event) {
+    event=cloneEvent(window.event);  
+  }
   POPMENUINPROGRESSELT = o;
   POPMENUINPROGRESSEVENT = event;
+  //  displayPropertyNames(POPMENUINPROGRESSEVENT);
   setTimeout('setonclick()',200); // wait 200ms before send request for menu
 }
 function aborddocmenu(event) {
@@ -20,12 +38,15 @@ function setonclick(event) {
 }
 
 function viewdocmenu(event,docid,onlyctrl,upobject,sourceobject) {
+  if (!event) event=POPMENUINPROGRESSEVENT;
+
   POPMENUINPROGRESSELT=false;
   var corestandurl=window.location.pathname+'?sole=Y&';
   var menuapp=MENUAPP;
   var menuaction=MENUACTION;
   var menuopt='';
-  var coord=false;
+  var coord=false;  
+  
   if (onlyctrl) menuopt='&onlyctrl=yes';
   else {
     if (ctrlPushed(event) && altPushed(event)) {
@@ -52,7 +73,6 @@ function viewdocsubmenu(event,docid,submenu,upobject) {
       menuaction='POPUPDOCDETAIL';
     }     
   }
- 
 
   var menuurl=corestandurl+'app='+menuapp+'&action='+menuaction+menuopt+'&id='+docid;
   viewsubmenu(event,menuurl,upobject);
