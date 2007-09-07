a72ff7eebf9b3fdc45ed00c75bfcd445947dfd15
eric <eric>
2007-09-07 14:29:50 +0000

    Add underline and strikeThrough button in simple bar of fckeditor

--- freedom/Zone/Fdl/fdl_fckeditor.js
+++ freedom/Zone/Fdl/fdl_fckeditor.js
@@ -1,14 +1,14 @@
 
 FCKConfig.ToolbarSets["Simple"] = [
 				   ['QuickSave','FontFormat','FontSize'],
-				   ['Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink'],
+				   ['Bold','Italic','Underline','StrikeThrough','-','OrderedList','UnorderedList','-','Link','Unlink'],
 				   '/',
 				   ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],['OrderedList','UnorderedList','-','Outdent','Indent'],
 				   ['TextColor','BGColor','Anchor','Table','-','FitWindow','-','Source','About']
 ] ;
 FCKConfig.ToolbarSets["DocAttr"] = [
 				   ['QuickSave','DocAttr','FontFormat','FontSize'],
-				   ['Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink'],
+				   ['Bold','Italic','Underline','StrikeThrough','-','OrderedList','UnorderedList','-','Link','Unlink'],
 				   '/',
 				   ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],['OrderedList','UnorderedList','-','Outdent','Indent'],
 				   ['TextColor','BGColor','Anchor','Table','-','FitWindow','-','Source','About']
