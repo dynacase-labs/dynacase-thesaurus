d6a16a1338102a9d4b27ca36ac26227ef0c9d080
eric <eric>
2007-10-09 16:43:37 +0000

    Add attachement

--- mailconnector/Class/Method.Mailbox.php
+++ mailconnector/Class/Method.Mailbox.php
@@ -8,6 +8,7 @@ function specRefresh() {
 
 
 function mb_testConnection() {
+  include_once("FDL/Lib.Vault.php");
   $login=$this->getValue("mb_login");
   $password=$this->getValue("mb_password");
   $server=$this->getValue("mb_servername");
@@ -76,12 +77,10 @@ function mb_parseMessage($msg) {
   $uid=$h->message_id;
   print "<b>".$this->mb_decode($h->subject)."</b> [$uid]";
   
-  //  print_r2($h);
+  // print_r2($h);
 
-  $body=imap_body ($this->mbox,$msg);
+  
   //	print("<b>$body</b>");
-  $o=imap_fetchstructure($this->mbox,$msg);
-  //  print_r2($o);
 
    $struct["subject"]=$this->mb_decode($h->subject);
    $struct["uid"]=$uid;
@@ -89,6 +88,7 @@ function mb_parseMessage($msg) {
    $struct["to"]=$this->mb_implodemail($h->to);
    $struct["from"]=$this->mb_implodemail($h->from);
    $struct["cc"]=$this->mb_implodemail($h->cc);
+   $struct["size"]=$h->Size;
 
 
    $status = imap_clearflag_full($this->mbox, $msg, "\\Seen");
@@ -96,6 +96,59 @@ function mb_parseMessage($msg) {
    $status = imap_setflag_full($this->mbox, $msg, '$label3');
 
 
+   $o=imap_fetchstructure($this->mbox,$msg);
+   print_r2($o);
+   if ($o->subtype=="PLAIN") {
+     $body=imap_body($this->mbox,$msg);
+     $struct["textbody"]=$body;
+   } else  if ($o->subtype=="HTML") {
+     $body=imap_body($this->mbox,$msg);
+     $struct["htmlbody"]=$body;
+     
+   
+   } else  if ($o->subtype=="ALTERNATIVE") {
+     if ($o->parts[0]->subtype=="PLAIN") {
+       $body1=imap_fetchbody($this->mbox,$msg,'1');
+       $struct["textbody"]=$body1;
+     }
+     if ($o->parts[1]->subtype=="HTML") {
+       $body=imap_fetchbody($this->mbox,$msg,'2');
+       $struct["htmlbody"]=$body;       
+     }
+   } else if ($o->subtype=="MIXED") {
+     foreach ($o->parts as $k=>$part) {
+       if ($part->subtype=="PLAIN") {
+	 $body=imap_fetchbody($this->mbox,$msg,sprintf("%d",$k+1));
+	 $struct["textbody"]=$body;
+       } else  if ($part->subtype=="HTML") {
+	 $body=imap_fetchbody($this->mbox,$msg,sprintf("%d",$k+1));
+	 $struct["htmlbody"]=$body;       
+       } else {
+	 if ($part->disposition=="INLINE") {
+	   $body=imap_fetchbody($this->mbox,$msg,sprintf("%d",$k+1));
+	   switch ($part->encoding) {
+	   case 3: // base64
+	     $body=imap_base64($body);
+	     break;
+	   case 0: // 7bit
+	     break;
+	   case 1: // 8bit
+	     break;
+	   case 2: //Binary
+	     break;
+	   case 4: // QUOTED-PRINTABLE
+	     break;
+	   case 5: // Others
+	     break;
+	   }
+
+	   $filename=uniqid("/var/tmp/_fdl").'.'.strtolower($part->subtype);
+	   $nc=file_put_contents($filename,$body);
+	   $struct["file"]=$filename;
+	 }
+       }
+     }
+   }
 
 
    $this->mb_createMessage($struct);
@@ -118,8 +171,6 @@ function mb_createMessage($msgStruct) {
   include_once("FDL/Lib.Dir.php");
   $uid=pg_escape_string($msgStruct["uid"]);
   $filter[]="emsg_uid='$uid'";
-
-  print_r2($msgStruct);
   $tdir=getChildDoc($this->dbaccess,0,"0",1,$filter,1,"LIST","EMESSAGE");
   if (count($tdir)==0) {
     $msg=createdoc($this->dbaccess,"EMESSAGE");    
@@ -128,10 +179,14 @@ function mb_createMessage($msgStruct) {
   }
   
   if ($msg) {
+      $msg->setValue("emsg_mailboxid",$this->id);
       $msg->setValue("emsg_uid",$msgStruct["uid"]);
       $msg->setValue("emsg_subject",$msgStruct["subject"]);
       $msg->setValue("emsg_from",$msgStruct["from"]);
       $msg->setValue("emsg_date",$msgStruct["date"]);
+      $msg->setValue("emsg_size",$msgStruct["size"]);
+      $msg->setValue("emsg_textbody",$msgStruct["textbody"]==""?' ':$msgStruct["textbody"]);
+      $msg->setValue("emsg_htmlbody",$msgStruct["htmlbody"]==""?' ':$msgStruct["htmlbody"]);
       $ttype=array();
       $tname=array();
       $tos=explode(';',$msgStruct["to"]);
@@ -151,6 +206,11 @@ function mb_createMessage($msgStruct) {
       
       $msg->setValue("emsg_sendtype",$ttype);
       $msg->setValue("emsg_recipient",$tname);
+
+
+
+      $err=$msg->storeFile('emsg_attach',$msgStruct["file"]);
+      print("<br>VAULTID<b>$err</b><br>");
 
       if ($msg->isAffected()) $err=$msg->Modify();
       else $err=$msg->Add();
