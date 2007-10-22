3a374ddfdcc7f097754ead588614c3b04220e7ab
marc <marc>
2007-10-22 07:20:53 +0000

    Use new global search (app fgsearch) for navigator search engine

--- freedom/Share/mozillaplugin/freedomsearch.php
+++ freedom/Share/mozillaplugin/freedomsearch.php
@@ -3,7 +3,7 @@
  * Search come from mozilla seach engine
  *
  * @author Anakeen 2005 
- * @version $Id: freedomsearch.php,v 1.5 2007/05/16 15:43:55 eric Exp $
+ * @version $Id: freedomsearch.php,v 1.6 2007/10/22 07:20:53 marc Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -17,12 +17,12 @@ $sensitive = GetHttpVars("sensitive", false);
 $viewone = GetHttpVars("viewone", "N");
 $keyword=GetHttpVars("keyword", "");
 if ($keyword=="") {
-  $location="index.php?sole=Y&&app=FREEDOM&action=FULLSEARCH";
+  $location="index.php?sole=Y&&app=FGSEARCH&action=FULLSEARCH";
   Header("Location: $location");
 } else {
   
 
-  $location="index.php?sole=Y&&app=FREEDOM&action=SEARCH&famid=$famid&latest=$latest&fromdir=$fromdir&sensitive=$sensitive&viewone=$viewone&keyword=$keyword";
+  $location="index.php?sole=Y&&app=FGSEARCH&action=SEARCH&famid=$famid&latest=$latest&fromdir=$fromdir&sensitive=$sensitive&viewone=$viewone&keyword=$keyword";
   Header("Location: $location");
 }
 ?>
