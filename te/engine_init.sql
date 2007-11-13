e9feb20120818b2e1a701e288f35e88dd7be396c
eric <eric>
2007-11-13 16:34:41 +0000

    Add script for txt2pdf

--- te/engine_init.sql
+++ te/engine_init.sql
@@ -10,3 +10,14 @@ INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'applicatio
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/msword', '/usr/share/pear/TE/engines/ooo2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'application/pdf', '/usr/share/pear/TE/engines/pdf2txt', NULL);
 INSERT INTO engine (name, mime, command, "comment") VALUES ('latin', 'text/html', '/usr/share/pear/TE/engines/html2txt', NULL);
+
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'application/vnd.oasis.opendocument.text', '/usr/share/pear/TE/engines/ooo2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'application/vnd.oasis.opendocument.presentation', '/usr/share/pear/TE/engines/ooo2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'application/vnd.oasis.opendocument.spreadsheet', '/usr/share/pear/TE/engines/ooo2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'application/vnd.oasis.opendocument.graphics', '/usr/share/pear/TE/engines/ooo2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'application/msword', '/usr/share/pear/TE/engines/ooo2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'application/vnd.sun.xml.writer', '/usr/share/pear/TE/engines/ooo2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'application/vnd.sun.xml.calc', '/usr/share/pear/TE/engines/ooo2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'application/vnd.sun.xml.impress', '/usr/share/pear/TE/engines/ooo2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'text/html', '/usr/bin/html2pdf', NULL);
+INSERT INTO engine (name, mime, command, "comment") VALUES ('pdf', 'text', '/usr/share/pear/TE/engines/txt2pdf', NULL);
