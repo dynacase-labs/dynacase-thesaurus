d49a823dda4c03c41d3a9eb33a39f3727ffe4e1b
eric <eric>
2007-11-22 17:07:02 +0000

    Begin to integrate image browser for FCKeditor

--- freedom/Zone/Fdl/fdl_fckeditor.js
+++ freedom/Zone/Fdl/fdl_fckeditor.js
@@ -4,7 +4,7 @@ FCKConfig.ToolbarSets["Simple"] = [
 				   ['Bold','Italic','Underline','StrikeThrough','-','OrderedList','UnorderedList','-','Link','Unlink'],
 				   '/',
 				   ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],['OrderedList','UnorderedList','-','Outdent','Indent'],
-				   ['TextColor','BGColor','Anchor','Table','-','FitWindow','-','Source','About']
+				   ['TextColor','BGColor','Anchor','Image','Table','-','FitWindow','-','Source','About']
 ] ;
 FCKConfig.ToolbarSets["DocAttr"] = [
 				   ['QuickSave','DocAttr','FontFormat','FontSize'],
