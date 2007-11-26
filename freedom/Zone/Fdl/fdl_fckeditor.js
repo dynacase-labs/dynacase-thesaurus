4a02a4ea94fe76d9c0bc7dd5814ced6a4e2c5fa1
eric <eric>
2007-11-26 17:08:24 +0000

    add special char

--- freedom/Zone/Fdl/fdl_fckeditor.js
+++ freedom/Zone/Fdl/fdl_fckeditor.js
@@ -1,17 +1,17 @@
 
 FCKConfig.ToolbarSets["Simple"] = [
 				   ['QuickSave','FontFormat','FontSize'],
-				   ['Bold','Italic','Underline','StrikeThrough','-','OrderedList','UnorderedList','-','Link','Unlink'],
+				   ['Bold','Italic','Underline','StrikeThrough','-','OrderedList','UnorderedList','-','SpecialChar','Link','Unlink'],
 				   '/',
 				   ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],['OrderedList','UnorderedList','-','Outdent','Indent'],
 				   ['TextColor','BGColor','Anchor','Image','Table','-','FitWindow','-','Source','About']
 ] ;
 FCKConfig.ToolbarSets["DocAttr"] = [
 				   ['QuickSave','DocAttr','FontFormat','FontSize'],
-				   ['Bold','Italic','Underline','StrikeThrough','-','OrderedList','UnorderedList','-','Link','Unlink'],
+				   ['Bold','Italic','Underline','StrikeThrough','-','OrderedList','UnorderedList','-','SpecialChar','Link','Unlink'],
 				   '/',
 				   ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],['OrderedList','UnorderedList','-','Outdent','Indent'],
-				   ['TextColor','BGColor','Anchor','Table','-','FitWindow','-','Source','About']
+				   ['TextColor','BGColor','Anchor','Image','Table','-','FitWindow','-','Source','About']
 ] ;
 
 
