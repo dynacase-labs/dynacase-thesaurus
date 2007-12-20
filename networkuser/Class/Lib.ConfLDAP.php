ef2d6d97c514264099d78e534207b36e706e8b54
eric <eric>
2007-12-20 08:54:21 +0000

    Add INET configuration for inetOrgPerson

--- networkuser/Class/Lib.ConfLDAP.php
+++ networkuser/Class/Lib.ConfLDAP.php
@@ -3,7 +3,7 @@
  * LDAP configuration
  *
  * @author Anakeen 2007
- * @version $Id: Lib.ConfLDAP.php,v 1.1 2007/01/31 17:48:24 eric Exp $
+ * @version $Id: Lib.ConfLDAP.php,v 1.2 2007/12/20 08:54:21 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM-AD
  */
@@ -26,6 +26,14 @@ function getLDAPconf($type,$attr=false) {
     $conf=array("LDAP_USERCLASS"=>"posixAccount",
 		"LDAP_USERLOGIN"=>"uid",
 		"LDAP_USERUID"=>"uidNumber",
+		"LDAP_GROUPCLASS"=>"posixGroup",
+		"LDAP_GROUPLOGIN"=>"cn",
+		"LDAP_GROUPUID"=>"gidNumber");
+    break;
+  case 'INET':
+    $conf=array("LDAP_USERCLASS"=>"inetOrgPerson",
+		"LDAP_USERLOGIN"=>"uid",
+		"LDAP_USERUID"=>"uid",
 		"LDAP_GROUPCLASS"=>"posixGroup",
 		"LDAP_GROUPLOGIN"=>"cn",
 		"LDAP_GROUPUID"=>"gidNumber");
