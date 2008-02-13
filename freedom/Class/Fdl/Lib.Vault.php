b6c236f5775cba1181c43119b0c3a309ce9be9ea
eric <eric>
2008-02-13 11:00:10 +0000

    Add convertFile function to convert file and wait the result

--- freedom/Class/Fdl/Lib.Vault.php
+++ freedom/Class/Fdl/Lib.Vault.php
@@ -3,7 +3,7 @@
  * Utilities functions for manipulate files from VAULT
  *
  * @author Anakeen 2007
- * @version $Id: Lib.Vault.php,v 1.19 2007/12/12 15:14:49 eric Exp $
+ * @version $Id: Lib.Vault.php,v 1.20 2008/02/13 11:00:10 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -176,6 +176,81 @@ function sendLatinTransformation($dbaccess,$docid,$attrid,$index,$vid) {
     } else {
       AddWarningMsg(_("TE engine activate but TE-CLIENT not found"));
     }
+  }
+  return $err;
+}
+
+/**
+ * send request to convert and waiting 
+ * @param string  $infile path to file to convert
+ * @param string  $engine engine name to use
+ * @param string  $outfile path where to store new file
+ * @param array &$info various informations for convertion process
+ * @return string error message 
+ */
+function convertFile($infile,$engine,$outfile,&$info) {
+  global $action;
+  if (file_exists($infile)  && ($engine!="")) {
+    $tea=getParam("TE_ACTIVATE");
+    if ($tea!="yes") return _("TE not activated");
+    
+    if (@include_once("TE/Class.TEClient.php")) {
+      
+      $callback="";
+      $ot=new TransformationEngine(getParam("TE_HOST"),getParam("TE_PORT"));
+      $err=$ot->sendTransformation($engine,$vid,$infile,$callback,$info);
+      if ($err=="") {
+	include_once("FDL/Class.TaskRequest.php");
+	$dbaccess = GetParam("FREEDOM_DB");
+	$tr=new TaskRequest($dbaccess);
+	$tr->tid=$info["tid"];
+	$tr->fkey=$vid;
+	$tr->status=$info["status"];
+	$tr->comment=$info["comment"];
+	$tr->uid=$action->user->id;
+	$tr->uname=$action->user->firstname." ".$action->user->lastname;
+	$err=$tr->Add();
+      }
+      if ($err=="") {
+	$tid=$info["tid"];
+	if ($tid == 0) $err=_("no task identificator");
+      }
+      // waiting response
+      if ($err=="") {
+	$status="";
+	ini_set("max_execution_time", "3600"); // may be long 
+	while (($status != 'K') && ($status != 'D') && ($err=="")) {
+	  $err=$ot->getInfo($tid,$info);
+	  $status=$info["status"];
+	  if ($err=="") {
+	    switch($info["status"]) {
+	    case  'P':
+	      $statusmsg=_("File:: Processing");
+	      break;
+	    case  'W':
+	      $statusmsg=_("File:: Waiting");
+	      break;
+	    case  'D':
+	      $statusmsg=_("File:: converted");
+	      break;
+	    case  'K':
+	      $statusmsg=_("File:: failed");
+	      break;
+	    default:
+	      $statusmsg=$info["status"];	
+	    }
+	  }
+	  
+	  sleep(2);
+	}
+	if (($err=="") && ($status=='D')) {
+	  include_once("FDL/insertfile.php");
+	  $err=getTEFile($tid,$outfile,$info);
+	}
+      }
+    }
+  } else {
+    $err= sprintf(_("file %s not found"),$infile);
   }
   return $err;
 }
