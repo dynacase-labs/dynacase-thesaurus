970ab9cad4406da81e66c118a3f98bd475225543
eric <eric>
2008-05-29 17:19:01 +0000

    Correct for gettitle

--- freedom/Class/Fdl/Class.DocCtrl.php
+++ freedom/Class/Fdl/Class.DocCtrl.php
@@ -3,7 +3,7 @@
  * Control Access Document
  *
  * @author Anakeen 2002
- * @version $Id: Class.DocCtrl.php,v 1.48 2008/05/27 13:48:59 eric Exp $
+ * @version $Id: Class.DocCtrl.php,v 1.49 2008/05/29 17:19:01 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  */
@@ -408,7 +408,7 @@ Class DocCtrl extends DocLDAP {
    */
   function ControlUp($uperm, $aclname) {
     if (isset($this->dacls[$aclname])) {
-      return (($uperm & (1 << ($this->dacls[$aclname]["pos"] ))) != 0 )?"":sprintf(_("no privilege %s for %s |%d]"),$aclname,$this->title,$this->id);
+      return (($uperm & (1 << ($this->dacls[$aclname]["pos"] ))) != 0 )?"":sprintf(_("no privilege %s for %s |%d]"),$aclname,$this->gettitle(),$this->id);
     } else {
       return sprintf(_("unknow privilege %s"),$aclname);
     }
