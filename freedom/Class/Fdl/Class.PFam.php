b2666a752ae41846cb8f903d60a62bd0efde8194
eric <eric>
2008-06-03 12:57:28 +0000

    Delete acl delete and edit for family profil

--- freedom/Class/Fdl/Class.PFam.php
+++ freedom/Class/Fdl/Class.PFam.php
@@ -3,7 +3,7 @@
  * Profil for family document
  *
  * @author Anakeen 2000 
- * @version $Id: Class.PFam.php,v 1.5 2006/04/03 14:56:26 eric Exp $
+ * @version $Id: Class.PFam.php,v 1.6 2008/06/03 12:57:28 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  */
@@ -16,12 +16,10 @@
 include_once("FDL/Class.Doc.php");
 
 
-Class PFam extends Doc
-{
+Class PFam extends Doc {
   // --------------------------------------------------------------------
   //---------------------- OBJECT CONTROL PERMISSION --------------------
-  var $acls = array("view","edit","delete","create","icreate");
-  // --------------------------------------------------------------------
+  var $acls = array("view","create","icreate");
   
   
   var $defDoctype='P';
@@ -31,9 +29,6 @@ Class PFam extends Doc
     // don't use Doc constructor because it could call this constructor => infinitive loop
      DocCtrl::__construct($dbaccess, $id, $res, $dbid);
   }
-
-
-
 }
 
 ?>
