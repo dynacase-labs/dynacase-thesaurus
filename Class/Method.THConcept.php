cd9d8d9d993798b0b22a2ae6e4383129af03aa9f
eric <eric>
2008-08-13 10:08:55 +0000

    ::getNarrowers

--- Class/Method.THConcept.php
+++ Class/Method.THConcept.php
@@ -89,7 +89,35 @@ function getLevel() {
   }
   return $level;
 }
+/**
+ * return recursive narrower
+ */
+function getRNarrowers() {
+  $pid=array();
+  $nrs=$this->getTValue("thc_narrower");
+  $nrsdoc=getDocsFromIds($this->dbaccess,$nrs,1);
 
+  foreach ($nrsdoc as $k=>$nr) {    
+    $nrs=array_merge($nrs, $this->_getRNarrowers(Doc::_val2array($nr['thc_narrower'])));
+    
+  }
+  return $nrs;
+}
+/**
+ * return recursive narrower
+ */
+private function _getRNarrowers($nrs) {
+  $_nrs=$nrs;
+ 
+
+  foreach ($nrs as $k=>$nr) {
+    $t=getTDoc($this->dbaccess,$nr);
+    $_nrs1=Doc::_val2array($t['thc_narrower']);
+    $_nrs2=$this->_getRNarrowers($_nrs1);
+    $_nrs=array_merge($_nrs,$_nrs1,$_nrs2);
+  }
+  return $_nrs;
+}
 
 /**
  * return localized label
