4ed4fad56765c7d62f725bf5d1c356478588e51e
eric <eric>
2008-11-20 13:38:12 +0000

    SKOS Analysis before importation

--- Action/th_skosimport.php
+++ Action/th_skosimport.php
@@ -3,7 +3,7 @@
  * Import SKOS thesaurus
  *
  * @author Anakeen 2000 
- * @version $Id: th_skosimport.php,v 1.8 2008/11/19 14:39:55 eric Exp $
+ * @version $Id: th_skosimport.php,v 1.9 2008/11/20 13:38:13 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage THESAURUS
@@ -22,10 +22,11 @@ function th_skosimport(&$action) {
   $uri = getHttpVars("thuri");
   $iduri = getHttpVars("_id_thuri");
   $newuri = getHttpVars("newthuri");
+  $analyze = (getHttpVars("analyze","yes")=="yes");
 
   global $_FILES;
   if (ini_get("max_execution_time") < MAXIMPORTTIME) ini_set("max_execution_time",MAXIMPORTTIME);
-  
+  $action->lay->set("msg2","");
   if (isset($_FILES["skos"])) {
     $filename=$_FILES["skos"]['name'];
     $skosfile=$_FILES["skos"]['tmp_name'];
@@ -39,47 +40,134 @@ function th_skosimport(&$action) {
   
   $desc=$doc->childNodes->item(0);
 
-  if ($iduri) {
-    $th=new_doc($dbaccess,$iduri);
-      print "UPDATE THESAURUS".$th->title."<br>\n";
-  } else {
-    if (! $newuri) $newuri=$desc->getAttribute("rdf:about");
-    if (! $newuri) $newuri="th_test";
-    $th=getThesaurusFromURI($dbaccess,$newuri);
-    if (! $th) {
-      // create it
-      $th=createDoc($dbaccess,"THESAURUS");
-      $th->setValue("thes_uri",$newuri);
-      $th->name=$newuri;
-      $err=$th->Add();
-      print "CREATE THESAURUS $uri<br>\n";
+  if ($analyze) { 
+    $concepts=$desc->childNodes;
+
+    $action->lay->set("msg",sprintf("%d concepts to import\n",$concepts->length));
+    $tr=array();
+    for( $j=0 ;  $j < $concepts->length; $j++ )  {       
+      $nod=$concepts->item($j);//Node j
+      $nodename=strtolower($nod->nodeName);
+      if ($nodename=="rdf:description") analyzeSkosConcept($dbaccess,$thid,$nod,$tr);        
     }
-  }
-  $thid=$th->id;
-  
+    $tul=array();
+    foreach ($tr as $k=>$v) {
+      $tul[$v['skos:broader']][]=$k;
+    }
+    //  print_r2($tul);
+    $tt=array();
+    foreach ($tul as $k=>$v) {
+      if ($k) {
+	foreach ($v as $kx=>$vx) {
+	  //      if (! is_array($tt[$k])) $tt[$k]=array();
+	  //print "tt[$k][$vx]<br/>";
+	  if (notxy($tt,$k,$vx)) {
+	    $tt[]=array($k);
+	    $tt[]=array($k,$vx);
+	  } else if (noty($tt,$vx)) th_insertafter($tt,$k,$vx);
+	  else th_insertbefore($tt,$k,$vx);
+	
+	}
+      }      
+    }
+    usort($tt,"th_order");
+    // print_r2($tr);
+    //print '<hr>';
+    foreach ($tt as $k=>$v) {
+      $id=array_pop($v);
+      $tlabel=$tr[$id]['skos:preflabel'];
+      $label='';
+      if (is_array($tlabel)) foreach ($tlabel as $kl=>$vl) $label.="($kl) $vl - ";
+      else $label="--------- NO LABEL --------";
+      $tout[]=array("level20"=> (count($v))*20,
+		    "level"=> (count($v))*1,
+		    "id"=>$id,
+		    "title"=>$label);
+    }
+    $action->lay->setBlockData("TDESC",$tout);
+    // print_r2($tt);
+  } else {
+    if ($iduri) {
+      $th=new_doc($dbaccess,$iduri);
+      $action->lay->set("msg2", _("UPDATE THESAURUS").' '.$th->title);
+    } else {
+      if (! $newuri) $newuri=$desc->getAttribute("rdf:about");
+      if (! $newuri) $newuri="th_test";
+      $th=getThesaurusFromURI($dbaccess,$newuri);
+      if (! $th) {
+	// create it
+	$th=createDoc($dbaccess,"THESAURUS");
+	$th->setValue("thes_uri",$newuri);
+	$th->name=$newuri;
+	$err=$th->Add();
+	$action->lay->set("msg2", _("CREATE THESAURUS").' '. $uri);
+      }
+    }
+    $thid=$th->id;  
+    $concepts=$desc->childNodes;
 
-  $concepts=$desc->childNodes;
+    $action->lay->set("msg", sprintf("%d concepts imported\n",$concepts->length));
 
-  print sprintf("%d concepts to imports\n",$concepts->length);
-
-  for( $j=0 ;  $j < $concepts->length; $j++ )  {       
+    for( $j=0 ;  $j < $concepts->length; $j++ )  {       
       $nod=$concepts->item($j);//Node j
       $nodename=strtolower($nod->nodeName);
       if ($nodename=="rdf:description") importSkosConcept($dbaccess,$thid,$nod);        
     }
 
-  // postImport Refreshing
-  refreshThConceptFromURI($dbaccess,$thid);
-  $th->refreshConcepts();
+    // postImport Refreshing
+    refreshThConceptFromURI($dbaccess,$thid);
+    $th->refreshConcepts();
+  }
+}
+function notxy($t,$x,$y) {
+  foreach ($t as $k=>$v) {
+    if (in_array($x,$v)) return false;
+    if (in_array($y,$v)) return false;
+  }
+  return true;
+}
+function noty($t,$y) {
+  foreach ($t as $k=>$v) {
+    if (in_array($y,$v)) return false;
+  }
+  return true;
 }
 
+// y before x
+function th_insertbefore(&$t,$x,$y) {
+  foreach ($t as $k=>$v) {
+    if (in_array($y,$v)) {
+      $t[$k]=array_merge((array)$x,$t[$k] );      
+    }
+  }
+  $t[]=array($x);
+}// y after x
+function th_insertafter(&$t,$x,$y) {  
+  foreach ($t as $k=>$v) {
+    if (in_array($x,$v)) {
+      $tt1=array();
+      foreach ($v as $kv=>$vv) {
+	$tt1[]=$vv;
+	if ($vv == $x) break;
+      }
+      $tt1[]=$y;
+      $t[]=$tt1;
+      break;
+    }
+  }
+}
+
+function th_order($a,$b) {
+  $sa=implode("-",$a);
+  $sb=implode("-",$b);
+  return strcmp($sa,$sb);
+}
 /**
- * import a concept
+ * Import a concept
  */
-function importSkosConcept($dbaccess,$thid,&$node) {
+function importSkosConcept($dbaccess,$thid,&$node,$analyze=false) {
   $tcol=array();
   $uri=$node->getAttribute("rdf:about");
-  print "URI:$uri\n";
 
   $co=getConceptFromURI($dbaccess,$uri);
   if (! $co) {
@@ -104,18 +192,12 @@ function importSkosConcept($dbaccess,$thid,&$node) {
       break;
     case "skos:broader":
       $refuri=$a->getAttribute("rdf:resource");
-      // $ref=getConceptIdsFromURI($dbaccess,$refuri);
-      //$co->setValue("thc_broader",$ref);
       $co->setValue("thc_uribroader",$refuri);
-      //      print "broader $refuri : $ref<br>\n";
       break;
-
     case "skos:narrower":
       break;
     case "skos:related":      
       $refuri=$a->getAttribute("rdf:resource");
-      //      $ref=getConceptIdsFromURI($dbaccess,$refuri);
-      //$co->setValue("thc_related",$ref);
       $trel[]=$refuri;
       $co->setValue("thc_urirelated",$trel);
       break;
@@ -140,21 +222,18 @@ function importSkosConcept($dbaccess,$thid,&$node) {
 	  $tcol[$lang]->setValue($aname,$nodevalue);
 	} else {
 	  $co->setValue($aname,$nodevalue);
-	  //	  print "$aname,$nodevalue<br>\n";
-	  
+	  //	  print "$aname,$nodevalue<br>\n";	  
 	}
       }
-      
     }
-
     //        print "$nodename<br>";
-
   }
   $err=$co->modify();
   //  $co->postModify();
   foreach ($tcol as $k=>$v) {
     $v->modify();
-  }  
+  }
+  return $err;
 }
 
 function refreshThConceptFromURI($dbaccess, $thid) {
@@ -169,4 +248,52 @@ function refreshThConceptFromURI($dbaccess, $thid) {
     $doc->modify();
   }
 }
+/**
+ * analyze a concept
+ */
+function analyzeSkosConcept($dbaccess,$thid,&$node,&$tcon) {
+  $tcol=array();
+  $uri=$node->getAttribute("rdf:about");
+
+  $tcon[$uri]=array();
+
+  $ats=$node->childNodes;
+
+  for( $j=0 ;  $j < $ats->length; $j++ )  {   
+    $a=$ats->item($j);
+    if ($a->nodeType == XML_TEXT_NODE) continue;
+    $lang=$a->getAttribute("xml:lang");
+    $nodename=strtolower($a->nodeName);
+    $nodevalue=utf8_decode($a->nodeValue);
+
+    switch ($nodename) {
+    case "rdfs:label":      
+      $tcon[$uri][$nodename]=$nodevalue;
+      break;
+    case "skos:broader":
+      $refuri=$a->getAttribute("rdf:resource");
+      $tcon[$uri][$nodename]=$refuri;
+      break;
+    case "skos:narrower":
+      break;
+    case "skos:related":      
+      $refuri=$a->getAttribute("rdf:resource");
+      $tcon[$uri][$nodename][]=$refuri;
+      break;
+    default:
+      if (preg_match("/skos:(.*)$/",$nodename,$reg)) {
+	$aname="thc_".$reg[1];	
+	if ($lang) {
+	  $tcon[$uri][$nodename][$lang]=$nodevalue;
+	 
+	} else {
+	  $tcon[$uri][$nodename][$lang]=$nodevalue;
+	}
+      }
+    }
+  }
+  
+  return $err;
+}
+
 ?>
