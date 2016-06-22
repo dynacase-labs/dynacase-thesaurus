4086245bdd4f026df4fa33fb4b43a182eaad95ab
Jérôme Augé <jerome.auge@anakeen.com>
2016-06-22 10:48:10 +0200

    refactor(license): Changed license to GNU Lesser General Public License v3.0 (refs #6282)

--- Action/th_editskosimport.php
+++ Action/th_editskosimport.php
@@ -1,7 +1,6 @@
 <?php
 /*
  * @author Anakeen
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FDL
 */
 /**
@@ -9,7 +8,6 @@
  *
  * @author Anakeen 2008
  * @version $Id: th_editskosimport.php,v 1.1 2008/09/15 16:08:49 eric Exp $
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FREEDOM
  * @subpackage THESAURUS
  */
