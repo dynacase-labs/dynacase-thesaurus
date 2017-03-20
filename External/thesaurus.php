4e5b7d6d4cf603ccde4ffc4ac851ea71c1ab88fe
Éric Brison <eric.brison@anakeen.com>
2017-03-20 10:31:20 +0100

    fix(thesaurus): help input (refs #6939)

--- External/thesaurus.php
+++ External/thesaurus.php
@@ -1,41 +1,44 @@
 <?php
 /*
  * @author Anakeen
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package THESAURUS
 */
 
 function getThConcept($dbaccess, $thesid, $name = '')
 {
-    $s1 = microtime(true);
-    include_once ("FDL/Class.SearchDoc.php");
-    
-    $s2 = microtime(true);
+    $th = new_doc($dbaccess, $thesid);
+    if (!$th->isAlive()) {
+        return sprintf(___("Thesaurus \"%s\" not exists", "thesaurus"), $thesid);
+    }
+
     $s = new SearchDoc($dbaccess, "THCONCEPT");
-    $s->addFilter("thc_thesaurus='" . intval($thesid) . "'");
-    //  $s->setObjectReturn();
-    if ($name) $s->addFilter("title ~* '" . pg_escape_string($name) . "'");;
-    $t = $s->search();
-    $s3 = sprintf("%.03f", microtime(true) - $s2);
+    $s->addFilter("thc_thesaurus='%d'", $th->initid);
+    if ($name) $s->addFilter("title ~* '%s' or thc_uribroader ~* '%s'", $name, $name);
+    $s->setObjectReturn(true);
+    $s->setOrder("thc_uribroader nulls first, title");
+    //$s->setSlice($limit);
+    $t = $s->search()->getDocumentList();
     $tr = array();
-    foreach ($t as $v) {
+    foreach ($t as $k => $v) {
         if ($v) {
+            if ($v->getRawValue("thc_uribroader")) {
+                $display = sprintf("<b><i>%s</i></b>/ %s", htmlspecialchars($v->getRawValue("thc_uribroader")) , htmlspecialchars($v->getTitle()));
+            } else {
+                $display = sprintf("<b>%s</b>", htmlspecialchars($v->getTitle()));
+            }
+            /**
+             * @var \Doc  $v
+             */
             $tr[] = array(
-                htmlspecialchars($v["title"], ENT_QUOTES),
-                $v["id"],
-                $v["title"]
+                $display,
+                $v->initid,
+                $v->getTitle()
             );
         }
     }
-    /*
-    while ($doc = $s->nextDoc()) {
-    $tr[] = array($doc->title, $doc->initid, $doc->title);      
-    }
-    */
+
     
-    $tr[] = array(
-        microtime(true) - $s1 . "[$s3]",
-        "-",
-        "-"
-    );
+
     return $tr;
 }
